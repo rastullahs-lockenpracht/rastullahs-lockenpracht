bcbab1fb86c4e10b2444d3d42a7e0362cf2f8173
Daniel Wickert <haffax@team-pantheon.de>
2004-10-11 20:19:22 +0000

    Zeilenenden.
    git-svn-id: svn://svn.berlios.de/dsa-hl@184 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/core/src/LightActor.cpp
+++ rl/trunk/engine/core/src/LightActor.cpp
@@ -23,41 +23,41 @@ LightActor::~LightActor()
 {
 	mSceneNode->detachObject(mLight->getName()); 
     mSceneNode->getCreator()->removeLight(mLight);
-}
-
-void LightActor::setAttenuation( Real range, Real constant, Real linear, Real quadric )
-{
-    mLight->setAttenuation( range, constant, linear, quadric );
-}
-
-void LightActor::setDiffuseColour( Real red, Real green, Real blue )
-{
-    mLight->setDiffuseColour(red,green,blue);
-}
-
-void LightActor::setDirection( Real x, Real y, Real z )
-{
-    mLight->setDirection(x,y,z);
-}
-
-void LightActor::setSpecularColour( Real red, Real green, Real blue )
-{
-    mLight->setSpecularColour(red,green,blue);
-}
-
-void LightActor::setSpotlightRange( Real innerangle, Real outerangle, Real falloff )
-{
-    mLight->setSpotlightRange(innerangle, outerangle, falloff );
-}
-
-void LightActor::setVisible( bool visible )
-{
-    mLight->setVisible( visible );
-}
-
-
+}
+
+void LightActor::setAttenuation( Real range, Real constant, Real linear, Real quadric )
+{
+    mLight->setAttenuation( range, constant, linear, quadric );
+}
+
+void LightActor::setDiffuseColour( Real red, Real green, Real blue )
+{
+    mLight->setDiffuseColour(red,green,blue);
+}
+
+void LightActor::setDirection( Real x, Real y, Real z )
+{
+    mLight->setDirection(x,y,z);
+}
+
+void LightActor::setSpecularColour( Real red, Real green, Real blue )
+{
+    mLight->setSpecularColour(red,green,blue);
+}
+
+void LightActor::setSpotlightRange( Real innerangle, Real outerangle, Real falloff )
+{
+    mLight->setSpotlightRange(innerangle, outerangle, falloff );
+}
+
+void LightActor::setVisible( bool visible )
+{
+    mLight->setVisible( visible );
+}
+
+
 const String& LightActor::getTypeName(){
 	return TYPENAME;
-}
-
-}
+}
+
+}
