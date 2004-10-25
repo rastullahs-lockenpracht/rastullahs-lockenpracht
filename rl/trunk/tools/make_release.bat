e0191ae24ff9326b7cb01489ae92a27a871f0089
Daniel Wickert <haffax@team-pantheon.de>
2004-10-25 22:44:46 +0000

    an neue Verzeichnisstruktur angepasst.
    git-svn-id: svn://svn.berlios.de/dsa-hl@240 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/tools/make_release.bat
+++ rl/trunk/tools/make_release.bat
@@ -12,47 +12,7 @@
 REM Verzeichnisse erzeugen
 REM ----------------------
 mkdir %RL_DIST%
-mkdir %RL_DIST%\media
-mkdir %RL_DIST%\script
-
-REM Binaries kopieren
-REM ---------------------
-copy %RL_ROOT%\engine\startup\bin\%BUILD%\Rastullah.exe %RL_DIST%\
-xcopy %RL_ROOT%\engine\startup\xml\*.* %RL_DIST%\ /I /K /Y /F
-copy %RL_ROOT%\engine\core\lib\%BUILD%\RlCore.dll %RL_DIST%\
-xcopy %RL_ROOT%\engine\core\scripts\*.* %RL_DIST%\script\ /I /K /Y /F
-copy %RL_ROOT%\engine\rules\lib\%BUILD%\RlRules.dll %RL_DIST%\
-copy %RL_ROOT%\engine\sound\lib\%BUILD%\RlSound.dll %RL_DIST%\
-copy %RL_ROOT%\engine\ui\lib\%BUILD%\RlUi.dll %RL_DIST%\
-copy %RL_ROOT%\engine\dialog\lib\%BUILD%\RlDialog.dll %RL_DIST%\
-copy %RL_ROOT%\engine\common\lib\%BUILD%\RlCommon.dll %RL_DIST%\
-
-REM Daten kopieren
-REM ---------------------
-copy %RL_ROOT%\engine\rules\data\* %RL_DIST%\
-
-REM Media kopieren
-REM ---------------------
-copy %RL_ROOT%\engine\rules\basis.xdi %RL_DIST%\
-copy %RL_ROOT%\modules\common\OgreCore.zip %RL_DIST%\media\
-xcopy %RL_ROOT%\modules\common\conf\*.* %RL_DIST%\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\fonts\*.** %RL_DIST%\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\gui\*.* %RL_DIST%\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\gui\media\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\sound\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\models\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\common\materials\*.* %RL_DIST%\media\ /I /K /Y /F
-
-xcopy %RL_ROOT%\modules\minidemo\conf\*.* %RL_DIST%\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\minidemo\maps\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\minidemo\models\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\minidemo\sound\*.* %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\minidemo\materials\*.* %RL_DIST%\media\ /I /K /Y /F
-
-xcopy %RL_ROOT%\modules\minidemo\scripts\*.* %RL_DIST%\script\ /I /K /Y /F
-
-xcopy %RL_ROOT%\modules\minidemo\dialogs\*.aiml %RL_DIST%\media\ /I /K /Y /F
-xcopy %RL_ROOT%\modules\minidemo\dialogs\*.xml %RL_DIST%\ /I /K /Y /F
+mkdir %RL_DIST%\logs
 
 REM Ogre Dateien kopieren
 REM ---------------------
@@ -65,12 +25,8 @@ copy %OGREPLUGINS%\NatureSceneManager\bin\%BUILD%\Plugin_NatureSceneManager.dll 
 copy %OGREPLUGINS%\BSPSceneManager\bin\%BUILD%\Plugin_BSPSceneManager.dll %RL_DIST%
 copy %OGREPLUGINS%\CgProgramManager\bin\%BUILD%\Plugin_CgProgramManager.dll %RL_DIST%
 copy %OGREPLUGINS%\OctreeSceneManager\bin\%BUILD%\Plugin_OctreeSceneManager.dll %RL_DIST%
-copy %OGREADDONS%\dotsceneoctree\PlugIns\DotSceneManager\bin\%BUILD%\Plugin_DotSceneManager.dll %RL_DIST%
 copy %OGREPLUGINS%\FileSystem\bin\%BUILD%\Plugin_FileSystem.dll %RL_DIST%
 copy %OGREPLUGINS%\ParticleFx\bin\%BUILD%\Plugin_ParticleFx.dll %RL_DIST%
-rem copy %OGREPLUGINS%\GuiElements\bin\%BUILD%\Plugin_GuiElements.dll %RL_DIST%
-
-rem copy %OGRERENDERERS%\Direct3D7\bin\%BUILD%\RenderSystem_Direct3D7.dll %RL_DIST%
 copy %OGRERENDERERS%\Direct3D9\bin\%BUILD%\RenderSystem_Direct3D9.dll %RL_DIST%
 copy %OGRERENDERERS%\GL\bin\%BUILD%\RenderSystem_GL.dll %RL_DIST%
 
@@ -88,19 +44,10 @@ REM Die restlichen Abhängigkeiten
 REM -----------------------------
 REM 
 
-REM TODO mit IFDEF oder sowas zwischen Debug und Release unterscheiden.
-REM copy %RL_ROOT%\Dependencies\openalpp\bin\openalpp.dll %RL_DIST%
-REM copy %RL_ROOT%\Dependencies\openthreads\bin\win32\OpenThreadsWin32.dll %RL_DIST%
-REM copy %RL_ROOT%\Dependencies\vorbis\bin\ogg.dll %RL_DIST%
-REM copy %RL_ROOT%\Dependencies\vorbis\bin\vorbis.dll %RL_DIST%
-REM copy %RL_ROOT%\Dependencies\vorbis\bin\vorbisenc.dll %RL_DIST%
-REM copy %RL_ROOT%\Dependencies\vorbis\bin\vorbisfile.dll %RL_DIST%
-
 copy %RL_ROOT%\Dependencies\openal\dll\*.dll %RL_DIST%
 copy %RL_ROOT%\Dependencies\openalpp\bin\*.dll %RL_DIST%
 copy %RL_ROOT%\Dependencies\openthreads\bin\win32\*.dll %RL_DIST%
 
-REM copy %RL_ROOT%\Dependencies\vorbis\bin\*.dll %RL_DIST%
 copy %RL_ROOT%\Dependencies\ode\lib\*.dll %RL_DIST%
 copy %RL_ROOT%\Dependencies\boost\lib\*.dll %RL_DIST%
 copy %RL_ROOT%\Dependencies\xerces\bin\*.dll %RL_DIST%
