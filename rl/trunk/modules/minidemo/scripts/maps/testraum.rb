1d14baa5ac9ba53a29303f4eb3b74ec18aee6efa
Sascha Kolewa <blakharaz@dsa-hl.de>
2004-10-31 00:04:03 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@259 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/modules/minidemo/scripts/maps/testraum.rb
+++ rl/trunk/modules/minidemo/scripts/maps/testraum.rb
@@ -9,9 +9,10 @@ pos = $World.getStartPoint()
 #heroActor.setPosition(pos[0], pos[1], pos[2])
 heroActor.setPosition(0.0, 20.0, 40.0)
 
-heroPerson = DsaManager.getSingleton().getPerson(10000)
+#heroPerson = DsaManager.getSingleton().getPerson(10000)
+heroPerson = Person.new(10000, "Alrik", "Der Testheld")
 PlayerSettings.preparePlayer(heroPerson)
 heroPerson.setActor(heroActor)
-	      
+
 $Ui.setActiveCharacter(heroPerson)
 
