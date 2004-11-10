bf97e7b9be7b66d0b6b46dca6a3f5e0f9c9cc8dd
Henrik Hinrichs <henrik@mutaphysis.de>
2004-11-10 20:56:56 +0000

    Minimale Optimierungen
    git-svn-id: svn://svn.berlios.de/dsa-hl@309 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/tools/JMap2Mesh/src/de/pantheon/tools/mapconverter/OgreMeshWriter.java
+++ rl/trunk/tools/JMap2Mesh/src/de/pantheon/tools/mapconverter/OgreMeshWriter.java
@@ -68,8 +68,8 @@ public class OgreMeshWriter
             if( textures.size() <= 0 )
                 break;
 
-            stream.append("<mesh>" + "\n");
-            stream.append("\t<submeshes>" + "\n");
+            stream.append("<mesh>\n");
+            stream.append("\t<submeshes>\n");
             
             // Jede Textur muss ein einzelnes Submesh sein
             for( int j=0; j < textures.size(); j++ )
@@ -86,8 +86,14 @@ public class OgreMeshWriter
                         subTriangles.add( tri );
                 }    	
                 
-                stream.append("\t\t<submesh material=\""+basename+"_"+texture+"\" usesharedvertices=\"false\" use32bitindexes=\"false\">"+"\n");
-                stream.append("\t\t\t<faces count=\""+subTriangles.size()+"\">"+"\n");
+                stream.append("\t\t<submesh material=\"");
+	            stream.append(basename);
+	            stream.append("_");
+	            stream.append(texture);
+	            stream.append("\" usesharedvertices=\"false\" use32bitindexes=\"false\">\n");
+                stream.append("\t\t\t<faces count=\"");
+	            stream.append(subTriangles.size());
+	            stream.append("\">\n");
                 
                 ArrayList vertices = new ArrayList();
                  // Alle Triangles
@@ -102,39 +108,59 @@ public class OgreMeshWriter
                     if( !vertices.contains( tri.getVec3() ) )
                         vertices.add( tri.getVec3() );
                     
-                    
-                    
-                    stream.append("\t\t\t\t<face v1=\""+vertices.lastIndexOf( tri.getVec1() )+"\" v2=\""+vertices.lastIndexOf( tri.getVec2() )+"\" v3=\""+vertices.lastIndexOf( tri.getVec3() )+"\" />"+"\n");  
+                    stream.append("\t\t\t\t<face v1=\"");
+    	            stream.append(vertices.lastIndexOf( tri.getVec1() ));
+    	            stream.append("\" v2=\"");
+    	            stream.append(vertices.lastIndexOf( tri.getVec2() ));
+    	            stream.append("\" v3=\""+vertices.lastIndexOf( tri.getVec3() ));
+    	            stream.append("\" />\n");  
                 }
                 
-                stream.append("\t\t\t</faces>"+"\n");
-                stream.append("\t\t\t<geometry vertexcount=\""+vertices.size()+"\">"+"\n");
-                stream.append("\t\t\t\t<vertexbuffer positions=\"true\" normals=\"true\" colours_diffuse=\"false\" texture_coords=\"1\" texture_coord_dimensions_0=\"2\" >"+"\n");
+                stream.append("\t\t\t</faces>\n");
+                stream.append("\t\t\t<geometry vertexcount=\"");
+	            stream.append(vertices.size());
+	            stream.append("\">\n");
+                stream.append("\t\t\t\t<vertexbuffer positions=\"true\" normals=\"true\" colours_diffuse=\"false\" texture_coords=\"1\" texture_coord_dimensions_0=\"2\" >\n");
                 
                 // Alle Vertices der Triangles
                 for( int k=0; k < vertices.size(); k++ )
                 { 
                     Vertex vert = (Vertex)vertices.get(k);
                     
-                    stream.append("\t\t\t\t\t<vertex>"+"\n");
-                    stream.append("\t\t\t\t\t\t<position x=\""+vert.getPosition().x()+"\" y=\""+vert.getPosition().y()+"\" z=\""+vert.getPosition().z()+"\" />"+"\n");
-                    stream.append("\t\t\t\t\t\t<normal x=\""+(vert.getNormal().x())+"\" y=\""+(vert.getNormal().y())+"\" z=\""+(vert.getNormal().z())+"\" />"+"\n");
-                    stream.append("\t\t\t\t\t\t<texcoord u=\""+vert.getU()+"\" v=\""+vert.getV()+"\" />"+"\n");
-                    stream.append("\t\t\t\t\t</vertex>"+"\n");
+                    stream.append("\t\t\t\t\t<vertex>\n");
+                    stream.append("\t\t\t\t\t\t<position x=\"");
+    	            stream.append(vert.getPosition().x());
+    	            stream.append("\" y=\"");
+    	            stream.append(vert.getPosition().y());
+    	            stream.append("\" z=\""+vert.getPosition().z());
+    	            stream.append("\" />\n");
+                    stream.append("\t\t\t\t\t\t<normal x=\"");
+    	            stream.append((vert.getNormal().x()));
+    	            stream.append("\" y=\"");
+    	            stream.append((vert.getNormal().y()));
+    	            stream.append("\" z=\"");
+    	            stream.append((vert.getNormal().z()));
+    	            stream.append("\" />\n");
+                    stream.append("\t\t\t\t\t\t<texcoord u=\"");
+    	            stream.append(vert.getU());
+    	            stream.append("\" v=\"");
+    	            stream.append(vert.getV());
+    	            stream.append("\" />\n");
+                    stream.append("\t\t\t\t\t</vertex>\n");
                 }
                 
-                stream.append("\t\t\t\t</vertexbuffer>"+"\n");
-                stream.append("\t\t\t</geometry>"+"\n");
-                stream.append("\t\t</submesh>"+"\n"); 
+                stream.append("\t\t\t\t</vertexbuffer>\n");
+                stream.append("\t\t\t</geometry>\n");
+                stream.append("\t\t</submesh>\n"); 
                 
             }
             
-            stream.append("\t</submeshes>"+"\n");    
-            stream.append("</mesh>"+"\n");
+            stream.append("\t</submeshes>\n");    
+            stream.append("</mesh>\n");
             
             System.out.println( "File written for Entity "+mesh_count+": "+outdir+"/"+basename+"_"+mesh_count+".xml" );            
             writeToFile( outdir+"/"+basename+"_"+mesh_count+".xml",stream.toString() );
-            
+            stream = null;
             mesh_count++;
         }    
         
@@ -148,20 +174,26 @@ public class OgreMeshWriter
             
             if( tex != null )
             {
-	            stream.append("material "+basename+"_"+textureName+"\n");
-	            stream.append("{"+"\n");
-	            stream.append("\ttechnique"+"\n");
-	            stream.append("\t{"+"\n");
-	            stream.append("\t\tpass"+"\n");
-	            stream.append("\t\t{"+"\n");
-	            stream.append("\t\t\ttexture_unit"+"\n");
-	            stream.append("\t\t\t{"+"\n");
-	            stream.append("\t\t\t\ttexture textures/"+textureName+"."+tex.getExtension()+"\n");
-	            stream.append("\t\t\t\tfiltering anisotropic anisotropic anisotropic"+"\n");
-	            stream.append("\t\t\t}"+"\n");
-	            stream.append("\t\t}"+"\n");
-	            stream.append("\t}"+"\n");
-	            stream.append("}"+"\n");
+	            stream.append("material ");
+	            stream.append(basename);
+	            stream.append("_");
+	            stream.append(textureName);
+	            stream.append("\n{\n");
+	            stream.append("\ttechnique\n");
+	            stream.append("\t{\n");
+	            stream.append("\t\tpass\n");
+	            stream.append("\t\t{\n");
+	            stream.append("\t\t\ttexture_unit\n");
+	            stream.append("\t\t\t{\n");
+	            stream.append("\t\t\t\ttexture textures/");
+	            stream.append(textureName);
+	            stream.append(".");
+	            stream.append(tex.getExtension());
+	            stream.append("\n\t\t\t\tfiltering anisotropic anisotropic anisotropic\n");
+	            stream.append("\t\t\t}\n");
+	            stream.append("\t\t}\n");
+	            stream.append("\t}\n");
+	            stream.append("}\n");
 	            numWritten++;
             }
         }
