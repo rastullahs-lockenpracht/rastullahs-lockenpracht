c445b9e3614cbff12e2ccbdd44b144faa6027ea3
Sascha Kolewa <blakharaz@dsa-hl.de>
2005-01-03 01:13:20 +0000

    Timerskripte eingefuehrt
    git-svn-id: svn://svn.berlios.de/dsa-hl@401 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/modules/common/scripts/classes.rb
+++ rl/trunk/modules/common/scripts/classes.rb
@@ -2,35 +2,14 @@ load "embed.rb"
 
 include RlScript
 
-class RastullahCommands < CommandExecutor
+class ScriptTask < TimerTask
 
-  public
-	  def getCommandsInBattle
-		  return getCommandsStartWith("cmdB_")
-	  end
+	def initialize(name)
+		super(name)
+	end
 	
-    def getCommandsOffBattle
-		  return getCommandsStartWith("cmdP_")
-	  end
-	
-    def getCommandsStartWith(start)
-	  	commands = Array.new
- 		  methods = self.class.private_instance_methods - self.class.superclass.private_instance_methods
- 		  methods.each { |method| commands.push(method) if method[0, start.length] == start }
-		  return commands
-	  end
-	
-    def executeCommand(command)
-		  send(command)
-	  end
-	
-	# und hier die Methoden hin, die aufgerufen werden sollen
-  private
-  
-    def cmdB_Angriff
-	  end
-	
-    def cmdP_Singen
-	  end
-	
+	def run(name)
+	  send(name)
+	end
+
 end
