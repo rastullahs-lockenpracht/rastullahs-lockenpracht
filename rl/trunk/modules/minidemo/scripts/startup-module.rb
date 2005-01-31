fb0314fb09025391a579062a7ca420e8d2668824
Jochen Schäfer <josch@joschs-robotics.de>
2005-01-31 00:54:17 +0000

    "MusicManager vorerst rausgenommen."
    git-svn-id: svn://svn.berlios.de/dsa-hl@489 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/modules/minidemo/scripts/startup-module.rb
+++ rl/trunk/modules/minidemo/scripts/startup-module.rb
@@ -1,8 +1,8 @@
 $Core.loadMap("Octree", "b_1.mesh", "testraum.rb");
 $World = $Core.getWorld();
 
-$MM.addSoundsIntoPlayList();
-$MM.setLooping(true);
-$MM.setAuto(true);
-$MM.playSong();
+#$MM.addSoundsIntoPlayList();
+#$MM.setLooping(true);
+#$MM.setAuto(true);
+#$MM.playSong();
 
