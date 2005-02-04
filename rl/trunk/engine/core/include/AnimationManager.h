79a0c3b7515210667c8a880d062ad6b311313f72
Henrik Hinrichs <henrik@mutaphysis.de>
2005-02-04 12:44:12 +0000

    Extensiv kommentiert
    git-svn-id: svn://svn.berlios.de/dsa-hl@550 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/core/include/AnimationManager.h
+++ rl/trunk/engine/core/include/AnimationManager.h
@@ -29,12 +29,18 @@ class Actor;
 class Animation;
 class TrackAnimation;
 
+/** 
+	Diese Klasse verwalte sämtliche Animationen und kümmert sich um Starten und Stoppen dieser
+	@see Animation, TrackAnimation
+*/
 class _RlCoreExport AnimationManager
   : public GameTask,
     protected Ogre::Singleton<AnimationManager>
 {
 public:
+	/// Interpolations-Modus, Spline ist besser, braucht auch mehr Perfomance
 	enum InterpolationMode { IM_LINEAR, IM_SPLINE };
+	/// RoatationsInterpolations-Modus, Spherical ist genauer, aber aufwändiger
 	enum RotationInterpolationMode { RIM_LINEAR, RIM_SPHERICAL };
 
     /** Default Constructor */
@@ -42,32 +48,63 @@ public:
 	/** Default Deconstructor */
     virtual ~AnimationManager();
 
+	/** Erzeugt eine Animation, trägt diese ein und beginnt
+		sofort mit dem Abspielen.
+		@param	animState Der AnimationState
+		@param	speed	Die Geschwindigkeit
+		@param	timesToPlay	Die Wiederholungen
+		@see Animation
+	*/
     Animation* addAnimation(Ogre::AnimationState* animState,
-        Ogre::Real speed=1.0, unsigned int timesToPlay=0 );
-        
+        Ogre::Real speed=1.0, unsigned int timesToPlay=0 ); 
+	/// Gibt die Animation zurück, die zum AnimationState gehört
 	Animation* getAnimation(Ogre::AnimationState* animState) const;
+	/// Entfernt eine Animation, und stoppt das Abspielen dieser
     void removeAnimation(Ogre::AnimationState* animState);
+	/** Erzeugt eine neue, leere TrackAnimation, der der SceneNode
+		des Actors zugeordnet wird.
+		@param	actor	Der zugeordnete Actor
+		@param	name	Der einzigartige Name der Animation
+		@param	length	Die gewünschte Länge des Tracks
+	*/
 	TrackAnimation* createTrackAnimation(Actor* actor,
 	    const Ogre::String& name, Ogre::Real length );
+	/// Entfernt eine Animation
 	void removeAnimation(Animation* anim);
+	///@todo Alle Animationen löschen, beim Szenen-Neuladen
+
 	
-	// Für globale SlowMotion oder anderes
+	/** Globale Beschleunigung, für SlowMotion oder andere sinnige Effekte
+		@param speed Der Beschleunigungsfaktor. 
+		@remarks Negative Werte lassen das Spiel nicht rückwärts laufen, nur
+			die Animationen.
+	*/
 	void setGlobalAnimationSpeed( Ogre::Real speed );
+	/// Gibt die globale Beschleunigung zurück
 	Ogre::Real getGlobalAnimationSpeed( ) const;
 	
+	/// Setzt den Standard InterpolationsModus für neue Animationen
 	void setDefaultInterpolationMode( AnimationManager::InterpolationMode im );
+	/// Gibt den Standard InterpolationsModus für neue Animationen zurück
 	AnimationManager::InterpolationMode getDefaultInterpolationMode() const;
 
+	/// Setzt den Standard RotationsInterpolationsModus für neue Animationen
 	void setDefaultRotationInterpolationMode( AnimationManager::RotationInterpolationMode rim );
+	/// Gibt den Standard RotationsInterpolationsModus für neue Animationen zurück
 	AnimationManager::RotationInterpolationMode getDefaultRotationInterpolationMode() const;
 
+	/// Geerbt von GameTask, wird in jedem Frame mit der vergangenen Zeit aufgerufen
     virtual void run(Ogre::Real timePassed);
 
+	/// Singleton
     static AnimationManager & getSingleton(void);
+	/// Singleton
 	static AnimationManager * getSingletonPtr(void);
 private:
+	/// Alle auszuführenden Animationen
     std::map<Ogre::AnimationState*,Animation*> mAnimationMap;
 
+	/// Die globale Beschleunigung
 	Ogre::Real mGlobalAnimationSpeed;
 };
 }
