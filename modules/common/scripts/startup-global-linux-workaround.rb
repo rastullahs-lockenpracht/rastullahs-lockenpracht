4cc6cf6c554e8ff86051e792e28b8080dd85f155
Jochen Schäfer <josch@joschs-robotics.de>
2005-02-18 18:04:58 +0000

    ""
    git-svn-id: svn://svn.berlios.de/dsa-hl@626 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/startup-global-linux-workaround.rb
+++
@@ -1,9 +0,0 @@
-# Beginn Original
-# $CORE.loadMap("Octree", "intro.mesh");
-# $World = $CORE.getWorld();
-# player = GameObject.new(10001, "Blah", "Blubb");
-# $UI.showMainMenu(player);
-# Ende Original
-
-$CORE.initializeModule("minidemo");
-load "startup-module.rb"
