88b2ea1bedf7a0078b46d9d321398dc3cc14dad0
Henrik Hinrichs <henrik@mutaphysis.de>
2005-04-14 12:22:05 +0000

    Intro umstrukturiert
    git-svn-id: svn://svn.berlios.de/dsa-hl@742 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/maps/intro.rb
+++ modules/common/scripts/maps/intro.rb
@@ -5,7 +5,7 @@ $CORE.log("skybox set");
 
 $CORE.log("Tageslicht erstellen..");
 sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
-sunlight.getControlledObject().setDirection(-0.8, -2.0, 3.0);
+sunlight.getControlledObject().setDirection(0.8, -1.5, 3.0);
 sunlight.getControlledObject().setCastShadows(false);
-sunlight.getControlledObject().setDiffuseColour(0.8,0.8,0.7);
+sunlight.getControlledObject().setDiffuseColour(0.9,0.9,0.8);
 $CORE.log("Tageslicht erstellt.");
