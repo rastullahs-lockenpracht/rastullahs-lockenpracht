e3f44927bfb883eee8f4a4bf948bb6aed422e703
Sascha Kolewa <blakharaz@dsa-hl.de>
2005-07-21 11:21:24 +0000

    Alrik jetzt mit Eigenschaften
    git-svn-id: svn://svn.berlios.de/dsa-hl@843 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/hero.rb
+++ modules/common/scripts/hero.rb
@@ -4,12 +4,19 @@ class Hero < Person
     def initialize
         super(10000, "Alrik", "Der Testheld");
         
+	setEigenschaft(1, 8);
+	setEigenschaft(2, 9);
+	setEigenschaft(3, 10);
+	setEigenschaft(4, 11);
+	setEigenschaft(5, 12);
         setEigenschaft(6, 13); #KO
-        setEigenschaft(7, 11); #KK
+        setEigenschaft(7, 14); #KK
         setWert(2, 10); # LE-Mod
         
         modifyLe(1000); # LeP = LE
-        modifyLe(-20); # 10 LeP abziehen
+	modifyAu(1000); # Au = voll
+	modifyLe(-20); # 10 LeP abziehen
+
 
         $CORE.log("Heldenaktor erstellen...");
         $heldaktor = $AF.createMeshActor("Held","held.mesh", PhysicsManager::GT_CAPSULE, -1.0);
