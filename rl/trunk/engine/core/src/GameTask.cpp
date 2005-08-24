8d94e4d4d55a72de656665d9ec2aba40e045ee7a
Sascha Kolewa <blakharaz@dsa-hl.de>
2005-08-24 11:14:05 +0000

    Lizenz auf Clarified Artistic License geändert, ergänzt, wo sie fehlt
    git-svn-id: svn://svn.berlios.de/dsa-hl@1025 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/core/src/GameTask.cpp
+++ rl/trunk/engine/core/src/GameTask.cpp
@@ -2,16 +2,16 @@
  * Copyright (C) 2003-2004 Team Pantheon. http://www.team-pantheon.de
  * 
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the Perl Artistic License.
+ *  it under the terms of the Clarified Artistic License.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  Perl Artistic License for more details.
+ *  Clarified Artistic License for more details.
  *
- *  You should have received a copy of the Perl Artistic License
+ *  You should have received a copy of the Clarified Artistic License
  *  along with this program; if not you can get it here
- *  http://www.perldoc.com/perl5.6/Artistic.html.
+ *  http://www.jpaulmorrison.com/fbp/artistic2.htm.
  */
 
 #include "GameTask.h"
--- rl/trunk/engine/sound/src/SoundManager.cpp
+++ rl/trunk/engine/sound/src/SoundManager.cpp
@@ -1,18 +1,18 @@
-/* SoundManager.cpp - Spielt verschiedene Sound nach Belieben.
- * (C) 2003-2005. Team Pantheon. www.team-pantheon.de
- * 
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the Perl Artistic License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  Perl Artistic License for more details.
- *
- *  You should have received a copy of the Perl Artistic License
- *  along with this program; if not you can get it here
- *  http://www.perldoc.com/perl5.6/Artistic.html.
- */
+/* This source file is part of Rastullahs Lockenpracht.
+* Copyright (C) 2003-2005 Team Pantheon. http://www.team-pantheon.de
+* 
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the Clarified Artistic License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  Clarified Artistic License for more details.
+*
+*  You should have received a copy of the Clarified Artistic License
+*  along with this program; if not you can get it here
+*  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+*/
 #include <set>
 #include "SoundManager.h"
 #include "SoundResource.h"
