bd2c73f61b8bcb05503948aa072a8de15671757a
Daniel Wickert <haffax@team-pantheon.de>
2005-08-25 08:58:07 +0000

    RlAssert1 repariert.
    git-svn-id: svn://svn.berlios.de/dsa-hl@1054 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/common/include/Exception.h
+++ rl/trunk/engine/common/include/Exception.h
@@ -63,7 +63,7 @@
     * werden soll, falls \c test \c false ist.
     */
 #  define RlAssert(test, msg) if(!(test)) Throw(AssertionFailedError, msg)
-#  define RlAssert1(test) RlAssert(AssertionFailedError, #test)
+#  define RlAssert1(test) RlAssert(test, #test)
    /**@brief Makro zur Absicherung von Annahmen.
     *
     * Wirft eine \c AssertionFailedException. N?tzlich um
