f5fcde656ee785419e2e64823b3e33d1cb660b94
Sascha Kolewa <blakharaz@dsa-hl.de>
2005-09-23 21:51:53 +0000

    Soundverzeichnisse
    git-svn-id: svn://svn.berlios.de/dsa-hl@1188 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/core/src/CoreSubsystem.cpp
+++ rl/trunk/engine/core/src/CoreSubsystem.cpp
@@ -251,7 +251,16 @@ namespace rl {
         addCommonSearchPath(moduleDir + "/dsa");
         addCommonSearchPath(moduleDir + "/maps");
         addCommonSearchPath(moduleDir + "/models");
-        addCommonSearchPath(moduleDir + "/sound");
+        addCommonSearchPath(moduleDir + "/sound"); //@todo ueber Verzeichnisnamen nachdenken
+		addCommonSearchPath(moduleDir + "/sound/holz");
+		addCommonSearchPath(moduleDir + "/sound/metall");
+		addCommonSearchPath(moduleDir + "/sound/natur");
+		addCommonSearchPath(moduleDir + "/sound/ost");
+		addCommonSearchPath(moduleDir + "/sound/sonst");
+		addCommonSearchPath(moduleDir + "/sound/waffen");
+		addCommonSearchPath(moduleDir + "/sound/wesen");
+		addCommonSearchPath(moduleDir + "/sound/zauber");
+		addCommonSearchPath(moduleDir + "/sound/mensch");
         addCommonSearchPath(moduleDir + "/gui");
         addCommonSearchPath(moduleDir + "/gui/fonts");
         addCommonSearchPath(moduleDir + "/gui/imagesets");
