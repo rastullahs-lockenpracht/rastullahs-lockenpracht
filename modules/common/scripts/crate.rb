9a4869cbb5a0838381818e63ff04ceaef47bd31d
Daniel Wickert <haffax@team-pantheon.de>
2005-09-24 22:27:16 +0000

    Kiste etwas schwerer gemacht.
    git-svn-id: svn://svn.berlios.de/dsa-hl@1206 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/crate.rb
+++ modules/common/scripts/crate.rb
@@ -4,7 +4,7 @@ class Crate < GameObject
   def initialize(name, description)
     super(name, description);
 
-    crateActor = $AM.createMeshActor( "crate", "kiste.mesh",  PhysicsManager::GT_BOX, 120.0 );
+    crateActor = $AM.createMeshActor( "crate", "kiste.mesh",  PhysicsManager::GT_BOX, 220.0 );
     $CORE.log("crate.rb - Aktor erstellt.");
     setActor(crateActor);
     $CORE.log("crate.rb - Aktor gesetzt");
