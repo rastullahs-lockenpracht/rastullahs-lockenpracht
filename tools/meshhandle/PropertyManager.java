8170838029b82e3748bff4ec22c29880148000ab
Matthias A. W. Ott <solidirid_farbenspiel@freenet.de>
2005-09-26 12:48:28 +0000

    Erster Upload - v0.6
    git-svn-id: svn://svn.berlios.de/dsa-hl@1213 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ tools/meshhandle/PropertyManager.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
+public class PropertyManager {
+	Properties pfade = new Properties();
+	public File[] propertiesLaden()
+	{
+		File propFile = new File (System.getProperty("user.dir"), "properties");
+		try {
+			FileInputStream propStream = new FileInputStream(propFile);
+			pfade.load(propStream);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Sie haben kein Property-File.", "Fehler beim Laden der Properties", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Keine Ahnung, was das schon wieder soll.", "Fehler beim Laden der Properties", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		File rl = new File(pfade.getProperty("RL-PFAD"));
+		if (!rl.isDirectory())
+		{
+			JOptionPane.showMessageDialog(null, "Geben Sie in den Properties ihr korrektes RL-Verzeichnis an.", "Fehler beim Laden der Properties", JOptionPane.ERROR_MESSAGE);
+		}
+		File ogre = new File(pfade.getProperty("CONVERTER"));
+		if (!rl.isDirectory())
+		{
+			JOptionPane.showMessageDialog(null, "Geben Sie in den Properties ihr Ogre-Tools-Verzeichnis korrekt an.", "Fehler beim Laden der Properties", JOptionPane.ERROR_MESSAGE);
+		}
+		File[] files = {rl, ogre};
+		return files;
+	}
+}
---
+++ tools/meshhandle/StartKlasse.java
@@ -0,0 +1,12 @@
+
+public class StartKlasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DialogKlasse dk = new DialogKlasse();
+		dk.main();
+	}
+
+}
