27375f1fea1dfeb16ac486ae8fb475b94b173c5d
Jochen Schäfer <josch@joschs-robotics.de>
2005-12-16 17:19:46 +0000

    "OpenAL-Klassen."
    git-svn-id: svn://svn.berlios.de/dsa-hl@1344 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ rl/branches/multimedia/engine/multimedia/src/openal/OalSoundChannel.cpp
@@ -0,0 +1,221 @@
+/* This source file is part of Rastullahs Lockenpracht.
+* Copyright (C) 2003-2005 Team Pantheon. http://www.team-pantheon.de
+* 
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the Clarified Artistic License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  Clarified Artistic License for more details.
+*
+*  You should have received a copy of the Clarified Artistic License
+*  along with this program; if not you can get it here
+*  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+*/
+
+#include "OalSoundChannel.h"
+#include <OgreMovableObject.h>
+#include <OgreAxisAlignedBox.h>
+#include <OgreVector3.h>
+#include "Sound.h"
+#include "SoundSubsystem.h"
+
+Ogre::String rl::OalSoundChannel::msMovableType = "OalSoundChannel";
+
+using namespace Ogre; 
+
+namespace rl
+{
+
+OalSoundChannel::OalSoundChannel(Sound *sound, const Ogre::String &name)
+ : SoundChannel(sound, name),
+   mChannel(NO_CHANNEL)
+{   
+}
+
+OalSoundChannel::~OalSoundChannel()
+{
+}
+
+/**
+ * @author JoSch
+ * @date 07-23-2005
+ */
+void OalSoundChannel::play() throw (RuntimeException)
+{
+    if (!getSound()->isValid())
+    {
+        getSound()->load();
+    }
+    // TODO setChannel(getSound()->createChannel());
+    setGain(255);
+    setPosition(Vector3(0.0, 0.0, 0.0));
+    setDirection(Vector3(0.0, 0.0, 0.0));
+    setVelocity(Vector3(0.0, 0.0, 0.0)); 
+    pause(false);
+}
+
+/**
+ * @author JoSch
+ * @date 07-04-2005
+ * @return Der Soundkanal
+ */
+const signed int OalSoundChannel::getChannel() const
+{
+    return mChannel;
+}
+
+/**
+ * @author JoSch
+ * @date 07-21-2005
+ * @param channel Der Soundkanal
+ */
+void OalSoundChannel::setChannel(signed int channel)
+{
+    mChannel = channel;
+}
+
+/**
+ * @author JoSch
+ * @date 03-11-2005
+ * @return Den Objekttypen
+ */
+const String& OalSoundChannel::getMovableType() const
+{
+    return msMovableType;
+}
+
+
+/**
+ * @return Die aktuelle Richtung der Soundquelle
+ * @author JoSch
+ * @date 07-23-2004
+ */
+const Vector3 OalSoundChannel::getDirection() const throw (RuntimeException)
+{
+    return mDirection;
+}
+
+/**
+ * @param direction Die neue Richtung der Soundquelle.
+ * @author JoSch
+ * @date 07-23-2004
+ */
+void OalSoundChannel::setDirection (const Vector3& direction) throw (RuntimeException)
+{
+    mDirection = direction;
+}
+
+/**
+ * @return Ist der Kanal gueltig?
+ * @author JoSch
+ * @date 08-05-2005
+ */
+bool OalSoundChannel::isValid() const throw (RuntimeException)
+{
+    return true;
+}
+
+/**
+ * @return Spielt die Soundquelle noch?
+ * @author JoSch
+ * @date 07-04-2005
+ */
+const bool OalSoundChannel::isPlaying() const
+{
+    return mIsPlaying;
+}
+
+
+/**
+ * @return Die aktuelle Position der Soundquelle
+ * @author JoSch
+ * @date 07-04-2005
+ */
+const Vector3 OalSoundChannel::getPosition() const throw (RuntimeException)
+{
+    return mPosition;
+}
+
+/**
+ * @param position Die neue Position der Soundquelle.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+void OalSoundChannel::setPosition(const Vector3& position) throw (RuntimeException)
+{
+    mPosition = position;
+}
+
+/**
+ * @return Die aktuelle Geschwindigkeit der Soundquelle
+ * @author JoSch
+ * @date 07-04-2005
+ */
+const Vector3 OalSoundChannel::getVelocity() const throw (RuntimeException)
+{
+    return mVelocity;
+}
+
+/**
+ * @param velocity Die neue Geschwindigkeit der Soundquelle.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+void OalSoundChannel::setVelocity(const Vector3& velocity) throw (RuntimeException)
+{
+    mVelocity = velocity;
+}
+
+/**
+ * @return Die aktuelle Lautstaerke der Soundquelle
+ * @author JoSch
+ * @date 07-04-2005
+ */
+const int OalSoundChannel::getGain() const throw (RuntimeException)
+{
+    return mGain;
+}
+
+/**
+ * @param gain Die neue Lautstarke der Soundquelle.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+void OalSoundChannel::setGain(const int gain) throw (RuntimeException)
+{
+    mGain = gain;
+}
+
+/**
+ * @param pausing TRUE l�sst den Sound unterbrechen.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+void OalSoundChannel::pause(bool pausing) throw (RuntimeException)
+{
+    mIsPlaying = !pausing;
+}
+
+/**
+ * @author JoSch
+ * @date 07-23-2004
+ */
+void OalSoundChannel::stop() throw (RuntimeException)
+{
+    mIsPlaying = false;
+}
+
+/**
+ * @return TRUE wenn der Sound unterbrochen wurde.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+bool OalSoundChannel::isPaused() throw (RuntimeException)
+{
+    return !mIsPlaying;
+}
+
+
+};
---
+++ rl/branches/multimedia/engine/multimedia/src/openal/OalSoundStream.cpp
@@ -0,0 +1,104 @@
+/* This source file is part of Rastullahs Lockenpracht.
+* Copyright (C) 2003-2005 Team Pantheon. http://www.team-pantheon.de
+* 
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the Clarified Artistic License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  Clarified Artistic License for more details.
+*
+*  You should have received a copy of the Clarified Artistic License
+*  along with this program; if not you can get it here
+*  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+*/
+#include "OalSoundStream.h"
+#include "OalSoundChannel.h"
+#include "SoundManager.h"
+
+
+using namespace Ogre;
+using namespace boost;
+
+namespace rl {
+ 
+
+/**
+ * @param name Der Name des Sounds.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+OalSoundStream::OalSoundStream(const String &name):
+    Sound(name)
+{
+}
+ 
+/**
+ * @param name Der Name des Sounds.
+ * @author JoSch
+ * @date 07-04-2005
+ */
+OalSoundStream::OalSoundStream(const SoundResourcePtr &soundres):
+    Sound(soundres)
+{
+}
+
+/**
+ * @author JoSch
+ * @date 07-04-2005
+ */
+OalSoundStream::~OalSoundStream()
+{
+    unload();
+}
+
+
+/**
+ * @author JoSch
+ * @date 07-12-2005
+ */
+void OalSoundStream::load() throw (RuntimeException)
+{
+    getSoundResource()->load(); // TODO Ist das wirklich nötig?
+}
+
+/**
+ * @author JoSch
+ * @date 07-22-2005
+ */
+void OalSoundStream::unload() throw (RuntimeException)
+{
+    getSoundResource()->unload();
+}
+
+
+/**
+ * @return TRUE wenn der Sound unterbrochen wurde.
+ * @author JoSch
+ * @date 07-12-2005
+ */
+bool OalSoundStream::isValid() const throw (RuntimeException)
+{
+    return true;
+}
+
+
+/**
+ * @return Der erzeugte Channel
+ * @author JoSch
+ * @date 08-08-2005
+ */
+SoundChannel *OalSoundStream::createChannel() throw (RuntimeException)
+{
+    OalSoundChannel *nullchannel = new OalSoundChannel(this, getName());
+    return nullchannel; 
+}
+
+
+void OalSoundStreamPtr::destroy()
+{
+    SharedPtr<OalSoundStream>::destroy();
+}
+
+} // Namespace
