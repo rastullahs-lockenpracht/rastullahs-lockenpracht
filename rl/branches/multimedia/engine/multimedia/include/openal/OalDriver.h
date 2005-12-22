dba8ab81328e55d66e11744baf9475e3629de369
Jochen Schäfer <josch@joschs-robotics.de>
2005-12-22 23:33:18 +0000

    "Soundkonfigurationsdialog begonnen. Soundtreiber-Klassen eingeführt"
    git-svn-id: svn://svn.berlios.de/dsa-hl@1360 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ rl/branches/multimedia/engine/multimedia/include/openal/OalDriver.h
@@ -0,0 +1,33 @@
+/* This source file is part of Rastullahs Lockenpracht.
+ * Copyright (C) 2003-2005 Team Pantheon. http://www.team-pantheon.de
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Clarified Artistic License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Clarified Artistic License for more details.
+ *
+ *  You should have received a copy of the Clarified Artistic License
+ *  along with this program; if not you can get it here
+ *  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+ */
+#ifndef OALDRIVER_H_
+#define OALDRIVER_H_
+
+#include "SoundDriver.h"
+
+namespace rl
+{
+
+class OalDriver : public rl::SoundDriver
+{
+public:
+	OalDriver();
+	virtual ~OalDriver();
+};
+
+}
+
+#endif /*OALDRIVER_H_*/
