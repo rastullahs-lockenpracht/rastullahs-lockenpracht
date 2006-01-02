885c9ccdd24c515858c438c19f487946da9de99f
Nils Gey <ich@nilsgey.de>
2006-01-02 01:09:56 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@1393 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/techdemo/scripts/maps/techdemo_quests.rb
+++ modules/techdemo/scripts/maps/techdemo_quests.rb
@@ -1,40 +1,30 @@
 questBook = RulesSubsystem.getSingleton().getQuestBook()
 
-unterkunftQuest = 
+spinneQuest = 
 	Quest.new(
-		"unterkunft",
-		"Eine Unterkunft",
-		"Ich brauche eine Unterkunft für die Nacht, vielleicht kann ich in dem Haus oder im Schuppen unterkommen.")
-questBook.addQuest(unterkunftQuest)
-unterkunftQuest.setState(Quest::OPEN)
-
-unterkunftQuest.addSubquest(
-	Quest.new(
-		"spinne", 
-		"Die Riesenspinne", 
-		"Auf der anderen Seite des Flusses lebt eine riesige Waldspinne. Arnolf Großschädel möchte, dass diese Spinne getötet wird. Erst danach bekomme ich ein Zimmer."))
-unterkunftQuest.addSubquest(
-	Quest.new(
-		"unterkunft_reden",
-		"Nochmal nach der Unterkunft fragen",
-		"Jetzt, da die Spinne tot ist, sollte doch einem trockenen Plätzchen nichts mehr im Wege stehen."))
+		"spinne",
+		"Eine Unterkunft und die Spinnen",
+		"Um beim Bauern unterzukommen muss ich eine Spinne auf der anderen Seite des Flusses töten.")
+questBook.addQuest(spinneQuest)
 
 hoehlenQuest = 
 	Quest.new(
 		"hoehle", 
 		"Die Räuberhöhle", 
-		"In einer Höhle hinter dem Haus soll es eine Höhle geben, die einst von Räubern als Unterschlupf benutzt wurde. Vielleicht liegen dort noch verborgene Schätze.")
+		"In einer Höhle in den Bergen, die einst von Räubern als Unterschlupf benutzt wurde, liegen noch verborgene Schätze.")
 questBook.addQuest(hoehlenQuest)
 
 hoehlenQuest.addSubquest(
 	Quest.new(
-		"hoehle_finden",
+		"hoehleEingang",
 		"Den Eingang finden",
-		"Als erstes muss ich den Eingang finden."))
+		"Als erstes muss ich den Eingang finden. Er ist eventuell verschüttet."))
 
 hoehlenQuest.addSubquest(
 	Quest.new(
-		"hoehle_truhe",
-		"Den Schatz finden",
-		"Irgendetwas vom Schatz muss doch noch zu finden sein."))
+		"hoehleZeug",
+		"In der Höhle Umsehen",
+		"Ich sollte mich in der Höhle umsehen und verschiedene Sachen ausprobieren."))
+
+RulesSubsystem.getSingleton().getQuestBook().getQuest("hoehleZeug").setPartsToDo(3)
 
