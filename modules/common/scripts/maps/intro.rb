93d6dfbb780c81b81f2f9100df685a0d42df8d1e
Henrik Hinrichs <henrik@mutaphysis.de>
2006-01-07 22:08:30 +0000

    Noch ein paar Sachen aus dem Wiki eingespielt
    git-svn-id: svn://svn.berlios.de/dsa-hl@1440 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/maps/intro.rb
+++ modules/common/scripts/maps/intro.rb
@@ -23,3 +23,11 @@ titelLied.getControlledObject().setLooping( true );
 $SCRIPT.log(" Abspielen");
 titelLied.getControlledObject().play();
 $SCRIPT.log("Sound fertig");
+
+
+sandsturm = $AM.createParticleSystemActor("sandsturm_1", "Rl/Sandsturm" );
+sandsturm.placeIntoScene( -125.482821, 99.060595, -2469.191551, 1.0, 0.0, 0.0, 0.0 );
+sandsturm = $AM.createParticleSystemActor("sandsturm_2", "Rl/Sandsturm" );
+sandsturm.placeIntoScene( 111.617033, -87.779894, -350.628781, 1.0, 0.0, 0.0, 0.0 );
+held = $AM.createMeshActor("KreisLaufHeld", "held.mesh" );
+held.placeIntoScene( -1125.412178, 742.069721, -1868.909645, 0.976916, 0.061914, 0.204045, -0.012932 );
