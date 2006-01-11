9e3c17357079e8b3fbdb348de08b1f6bd2a1ead0
Sascha Kolewa <blakharaz@dsa-hl.de>
2006-01-11 22:00:33 +0000

    Hauptmenü-Map nur laden, wenn auch Hauptmenü gefordert ist
    git-svn-id: svn://svn.berlios.de/dsa-hl@1493 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/startup-global-mainmenu.rb
+++ modules/common/scripts/startup-global-mainmenu.rb
@@ -1,5 +1,9 @@
 load 'embed.rb'
 
+$SCRIPT.log("Loading menu background..")
+$CORE.loadMap("Octree", "intro.scene", "General", "intro.rb");
+$SCRIPT.log("done")
+
 $SCRIPT.log("showMainMenu()..")
 $UI.showMainMenu()
 $SCRIPT.log("done.")
--- modules/common/scripts/startup-global.rb
+++ modules/common/scripts/startup-global.rb
@@ -1,10 +1,5 @@
 load 'embed.rb'
 load 'global-settings.rb'
 
-$SCRIPT.log("Loading menu background..")
-$CORE.loadMap("Octree", "intro.scene", "General", "intro.rb");
-$SCRIPT.log("done")
-$World = $CORE.getWorld();
-
 GlobalSettings.addIngameActions(ActionManager.getSingleton())
 $UI.update()
