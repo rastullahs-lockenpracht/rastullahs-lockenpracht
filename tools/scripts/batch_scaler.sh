d284048394e93794c26e1e8a16a8c9d0eb7e705e
Daniel Wickert <haffax@team-pantheon.de>
2006-01-30 23:52:40 +0000

    Batch-Skalierskript
    git-svn-id: svn://svn.berlios.de/dsa-hl@1612 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ tools/scripts/batch_scaler.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# Erstes Argument: Skalierungsfaktor
+# Zweites Argument: Zielverzeichnis (muss bereits exisiteren)
+find . -name "*.skeleton" -exec OgreXmlConverter \{\} \;
+find . -name "*.mesh" -exec OgreXmlConverter \{\} \;
+
+find . -maxdepth 1 -name "*.skeleton.xml" -print -exec java -cp . meshhandle.SkeletonScaler \{\} $1 $2/\{\} \;
+find . -maxdepth 1 -name "*.mesh.xml" -print -exec java -cp . meshhandle.MeshScaler \{\} $1 $2/\{\} \;
+find . -maxdepth 1 -name "*.scene" -print -exec java -cp . meshhandle.SceneScaler \{\} $1 $2/\{\} \;
+
+cd neu
+find . -name "*.skeleton.xml" -exec OgreXmlConverter \{\} \;
+find . -name "*.mesh.xml" -exec OgreXmlConverter \{\} \;
+cd ..
