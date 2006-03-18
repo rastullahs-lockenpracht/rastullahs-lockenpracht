f74f1273249a3e6b86b94678e74277519276be12
Sascha Kolewa <blakharaz@dsa-hl.de>
2006-03-18 00:28:14 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@1710 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ rl/trunk/engine/ui/src/MainMenuEngineWindow.cpp
@@ -0,0 +1,37 @@
+/* This source file is part of Rastullahs Lockenpracht.
+ * Copyright (C) 2003-2005 Team Pantheon. http://www.team-pantheon.de
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Clarified Artistic License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Clarified Artistic License for more details.
+ *
+ *  You should have received a copy of the Clarified Artistic License
+ *  along with this program; if not you can get it here
+ *  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+ */
+#include <boost/bind.hpp>
+
+#include "UiPrerequisites.h"
+
+#include "ConfigurationManager.h"
+#include "MainMenuEngineWindow.h"
+
+using namespace CEGUI;
+using namespace Ogre;
+
+namespace rl {
+
+	MainMenuEngineWindow::MainMenuEngineWindow() :
+		CeGuiWindow("mainmenuenginewindow.xml", WND_SHOW, true)
+	{
+
+		getWindow("MainMenu/EngineVersion")->setText(
+			ConfigurationManager::getSingleton().getEngineVersionString()+
+			" ("+StringConverter::toString(ConfigurationManager::getSingleton().getEngineBuildNumber())+")");
+
+	}
+}
