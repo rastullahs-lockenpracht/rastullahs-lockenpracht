498554a7c5267181656f9f8c3f89b91351bcbb11
Daniel Wickert <haffax@team-pantheon.de>
2006-03-19 17:10:31 +0000

    Reichweite für Aktionen wieder limitiert.
    git-svn-id: svn://svn.berlios.de/dsa-hl@1758 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/ui/src/InputManager.cpp
+++ rl/trunk/engine/ui/src/InputManager.cpp
@@ -599,12 +599,7 @@ namespace rl {
 
     void InputManager::updatePickedObject(float mouseRelX, float mouseRelY)
     {
-		// -----------------------------------------------------------------------------------------
-		// Geändert von ChaosBlender
-		// am 11.März 2006 (irgendwann nachts)
-		// Änderung: Den Wert length auf 5 reduziert. Der Wert sollte sich zwischen 5 und 8 befinden
-		// -----------------------------------------------------------------------------------------
-        Actor* actor = ActorManager::getSingleton().getActorAt(mouseRelX, mouseRelY, 30, 50);
+        Actor* actor = ActorManager::getSingleton().getActorAt(mouseRelX, mouseRelY, 30, 7);
         /*DebugWindow::getSingleton().setText(
             "X="+StringConverter::toString(mouseRelX)+
             "   Y="+StringConverter::toString(mouseRelY)+
