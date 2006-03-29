6df59b22da71f9bc19d166309406d9074fc0f71d
Henrik Hinrichs <henrik@mutaphysis.de>
2006-03-29 15:01:53 +0000

    Partner und so nicht vertauschen
    git-svn-id: svn://svn.berlios.de/dsa-hl@1837 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/minidemo/scripts/dialogdemo.rb
+++ modules/minidemo/scripts/dialogdemo.rb
@@ -6,21 +6,27 @@ print( "DialogDemo wird geladen" );
 print( "Definiere Listener" );
 # Definition des GameAreaListeners
 class DialogListener < GameAreaListener
-	def areaEntered(anEvent)
+	def initialize(source)	
+		super();
 		# Load a bot from a bot definition xml file
-		$dialogChar = $DS.loadBot( "Alrik.xml" );
-		# Set the game objects for the dialogcharacter and its dialog partner
-		$dialogChar.setDialogCharacter( anEvent.getSource().getActor.getGameObject() );
-		$dialogChar.setDialogPartner( anEvent.getProvokingActor().getGameObject() );
-		print( anEvent.getProvokingActor().getName() + " moechte mit " + anEvent.getSource().getActor().getName() + " reden" );
-		$WM.showDialog( $dialogChar );
+		@dialogChar = $DS.loadBot( "Alrik.xml" );
+		@dialogChar.setDialogPartner( source.getGameObject() );
 	end
+	
+	def areaEntered(anEvent)
+		# Set the game objects for the dialogcharacter and its dialog partner
+		@dialogChar.setDialogCharacter( anEvent.getProvokingActor().getGameObject() );
+		print( anEvent.getProvokingActor().getName() + " moechte mit " + anEvent.getSource().getActor().getName() + " reden" );
+		$WM.showDialog( @dialogChar );
+	end
+
 	def areaLeft(anEvent)
 		print( "Dialogpartner ist außerhalb der Reichweite" );
 	end
 end
 
 $SCRIPT.log("Binde DialogListener an KreislaufHeld");
-$GameEveMgr.addSphereAreaListener( $AM.getActor( "KreisLaufHeld" ), 0.5, DialogListener.new, Actor::QGF_PLAYER );
+kreislauf = $AM.getActor( "KreisLaufHeld" );
+$GameEveMgr.addSphereAreaListener( kreislauf, 2.0, DialogListener.new(kreislauf), Actor::QGF_PLAYER );
 
 print( "DialogDemo fertig geladen" );
