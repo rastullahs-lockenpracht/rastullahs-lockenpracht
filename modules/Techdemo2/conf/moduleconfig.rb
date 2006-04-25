87fd0a4407ba1bc2839b0270a12e70d6e41a39e4
Simon Klein <pnyx1@gmx.net>
2006-04-25 15:21:39 +0000

    Techdemo2 module eingerichtet
    git-svn-id: svn://svn.berlios.de/dsa-hl@1929 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ modules/Techdemo2/conf/moduleconfig.rb
@@ -0,0 +1,30 @@
+include RlScript
+
+class Techdemo2 < ContentModule
+    def initialize()
+        super("Techdemo2", "Techdemo 2", false, 200603030);
+    end
+
+    def getDependencies()
+        return ["common"];
+    end
+
+    def getTextureLocations()
+	return ["textures"];
+    end
+
+    def getModelLocations()
+	return []
+    end
+
+    def getSoundLocations()
+	return []
+    end
+
+    def start()
+        $CORE.loadMap("Octree", "Techdemo2.scene", "Techdemo2", "Techdemo2.rb");
+        $World = $CORE.getWorld();
+    end
+end
+
+CoreSubsystem.getSingleton().registerModule(Techdemo2.new());
---
+++ modules/Techdemo2/scripts/maps/Techdemo2.rb
@@ -0,0 +1,35 @@
+load "embed.rb"
+
+require 'player.rb'
+require 'hero.rb'
+
+# Physik aktivieren
+$PM.setEnabled(true);
+
+$SCRIPT.log("init map 'Techdemo2'...");
+$World = $CORE.getWorld()
+$World.setSkyBox(true, "rl/dsa07", 100.0)
+$SCRIPT.log("skybox set");
+
+$SCRIPT.log("Tageslicht erstellen..");
+sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
+sunlight.getControlledObject().setDirection(0.7, -2.0, 0.8);
+sunlight.getControlledObject().setCastShadows(false);
+sunlight.getControlledObject().setDiffuseColour(0.9,0.9,0.8);
+$SCRIPT.log("Tageslicht erstellt.");
+
+$SCRIPT.log("Held erstellen");
+$hero = Hero.new;
+$SCRIPT.log("Held erstellt");
+$SCRIPT.log("Held in die Szene einfuegen.");
+$hero.getActor().placeIntoScene(0.0, 0.0, 13.0, 1.0, 0.0, 0.0, 0.0);
+$SCRIPT.log("Held eingefügt.");
+PlayerSettings.preparePlayer($hero);
+$SCRIPT.log("Held vorbereitet.");
+$UI.setActiveCharacter($hero);
+$SCRIPT.log("Held als aktiver Charakter gesetzt.");
+
+$World.setFog( World::FOG_EXP, [0.8,0.8,1.0,0.5], 0.00103, 0.4, 1.0);
+
+$SCRIPT.log("map 'Techdemo2' initialisiert.");
+$WF.toggleCharacterStateWindow()
