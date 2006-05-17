44b5c60b2c198e4057993ffbd54b9c7350d2d594
Simon Klein <pnyx1@gmx.net>
2006-05-17 21:26:07 +0000

    Techdemo2 update - neue lightingmap, alle Bäume die da sein sollen sind es auch, neuer sky, dunklerer farn
    git-svn-id: svn://svn.berlios.de/dsa-hl@1979 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/maps/Techdemo2.rb
+++ modules/Techdemo2/scripts/maps/Techdemo2.rb
@@ -8,14 +8,14 @@ $PM.setEnabled(true);
 
 $SCRIPT.log("init map 'Techdemo2'...");
 $World = $CORE.getWorld()
-$World.setSkyBox(true, "rl/dsa07", 100.0)
+$World.setSkyBox(true, "rl/desert07", 100.0)
 $SCRIPT.log("skybox set");
 
 $SCRIPT.log("Tageslicht erstellen..");
 sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
 sunlight.getControlledObject().setDirection(-0.17101, -0.873647, 0.969846);
 sunlight.getControlledObject().setCastShadows(true);
-sunlight.getControlledObject().setDiffuseColour(0.9,0.9,0.8);
+sunlight.getControlledObject().setDiffuseColour(1.0,0.7,0.7);
 $SCRIPT.log("Tageslicht erstellt.");
 
 #shadowlight = $AM.createLightActor("shadowlight", LightObject::LT_SPOTLIGHT);
@@ -36,7 +36,7 @@ $SCRIPT.log("Held vorbereitet.");
 $UI.setActiveCharacter($hero);
 $SCRIPT.log("Held als aktiver Charakter gesetzt.");
 
-$World.setFog( World::FOG_EXP, [0.8,0.8,1.0,0.5], 0.00103, 0.4, 1.0);
+$World.setFog( World::FOG_EXP, [0.15,0.1,0.1,0.5], 0.00503, 0.4, 1.0);
 
 #******** mapchange **********
 $SCRIPT.log("Mapchange: Kugel-Zentrum Actor erstellen");
