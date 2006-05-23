24322688d580146ca0e8232ee0e3927e0391b1e2
Daniel Wickert <haffax@team-pantheon.de>
2006-05-23 12:29:01 +0000

    API-Dokumentation fuer die Page-Schnittstelle in DebugWindow nachgereicht.
    git-svn-id: svn://svn.berlios.de/dsa-hl@1991 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/ui/include/DebugWindow.h
+++ rl/trunk/engine/ui/include/DebugWindow.h
@@ -37,13 +37,41 @@ namespace rl {
         static DebugWindow* getSingletonPtr(void);
 
 		void setVisible(bool visible, bool destroyAfterHide = false);
-        void registerPage(const Ogre::String& page);
-        void unregisterPage(const Ogre::String& page);
 
+        /** Registers a page on the debug window.
+         *  Debug Window allows multiple pages of informations to be registered
+         *  This allows components to show debug informations without meddling with
+         *  other components debug text.
+         *  Only one page at a time is shown.
+         *  @param name the key and caption of the registered page.
+         *  @throw IllegalArgumentException if there already is a page with this name registered.
+         *  @see unregisterPage, setPageText, showNextPage
+         */
+        void registerPage(const Ogre::String& name);
+
+        /** Unregisters a page on the debug window.
+         *  This page is removed from the DebugWindow and setting its text is no longer allowed.
+         *  @param name the key of the to be unregistered page.
+         *  @throw IllegalArgumentException if there is no page with this name.
+         *  @see registerPage, setPageText, showNextPage
+         */
+        void unregisterPage(const Ogre::String& name);
+
+        /** Unregisters a page on the debug window.
+         *  This page is removed from the DebugWindow and setting its text is no longer allowed.
+         *  @param page the key of the page, whose text shall be set.
+         *  @param text the text to be shown on the page. Use '\n' to break it into multiple lines.
+         *  @throw IllegalArgumentException if there is no page with this name.
+         */
         void setPageText(const Ogre::String& page, const Ogre::String& text);
+
+        /** Sets the text for the one line StaticText field at the bottom of the window.
+         *  This is used for situational short term info.
+         */
         void setMessageText(const Ogre::String& text);
 		void run(Ogre::Real elapsedTime);
 
+        /// Changes the shown page to the next page in the list.
         void showNextPage();
 
     private:
