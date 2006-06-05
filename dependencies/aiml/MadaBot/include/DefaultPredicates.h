d97a264f0cfb10e88a959039d9aff07df10aef80
Philipp Walser <mail@philippw.de>
2006-06-05 10:01:23 +0000

    added defaultpredicate
    git-svn-id: svn://svn.berlios.de/dsa-hl@2055 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ dependencies/aiml/MadaBot/include/DefaultPredicates.h
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2006 Philipp Walser
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef DEFAULT_PREDICATES_H
+#define DEFAULT_PREDICATES_H
+
+#include "Predicates.h"
+
+namespace MadaBot
+{
+	/**
+	 * Attributes/Properties/Abilities and other variables of a bot
+	 */
+	template <class S> class DefaultPredicates : public Predicates<S>
+	{
+	public:
+		/**
+		 * Getter, should be templatized, but then it couldn't be virtual
+		 * maybe return string and use lexical cast from boot in ConditionProcessor
+		 */
+		S getPredicate(const S& pName) const;
+
+		void setPredicate(const S& pName, const S& pValue);
+		
+		S getType()const;
+	protected:
+		std::map<S,S> mValues;
+	};
+
+	template <class S> S DefaultPredicates::getPredicate(const S& pName) const
+	{
+		std::map<S,S>::const_iterator itr = mValues.find(pName);
+		if(itr != mValues.end())
+		{
+			return itr.second;
+		}
+		//throw exception
+		return "";
+	}
+
+	template <class S> void DefaultPredicates::setPredicate(const S& pName, const S& pValue)
+	{
+		mValues[pName] = pValue;
+	}
+
+	template <class S> S DefaultPredicates::getType() const
+	{
+		return "default";
+	}
+}
+#endif
