d74029fffa67283200fad276b3fb48f187cfcf30
Philipp Walser <mail@philippw.de>
2006-07-05 17:36:37 +0000

    added (more) steering behaviours to npcs
    git-svn-id: svn://svn.berlios.de/dsa-hl@2290 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/sonja.rb
+++ modules/Techdemo2/scripts/sonja.rb
@@ -7,8 +7,15 @@ print( "Sonja Bot wird geladen" );
 print( "Definiere Listener" );
 
 sonja = Creature.new("Sonja", "Sonja");
-sonja.setActor($AM.createMeshActor("Sonja", "held.mesh" ));
+sonja.setActor($AM.createMeshActor("Sonja", "men_Gennisch.mesh", PhysicsManager::GT_ELLIPSOID, 75.0));
 sonja.getActor().placeIntoScene([-26.7, -6.6, 22.9]);
+
+bot = $DS.loadBot("Sonja", "sonja.xml");
+bot.setNonPlayerCharacter(sonja);
 sonja.addAction(TalkAction.new("Sonja", "sonja.xml"))
+agent = $AI.createAgent(bot);
+agent.addSteeringBehaviour(DefaultWanderBehaviour.new);
+agent.addSteeringBehaviour(AvoidObstaclesBehaviour.new);
+agent.addSteeringBehaviour(DialogBehaviour.new($hero.getActor()));
 
 print( "Sonja Bot fertig geladen" );
--- modules/Techdemo2/scripts/tsafried.rb
+++ modules/Techdemo2/scripts/tsafried.rb
@@ -7,8 +7,15 @@ print( "Tsafried Bot wird geladen" );
 print( "Definiere Listener" );
 
 tsafried = Creature.new("Tsafried", "Tsafried");
-tsafried.setActor($AM.createMeshActor("Tsafried", "held.mesh" ));
+tsafried.setActor($AM.createMeshActor("Tsafried", "men_Gennisch.mesh", PhysicsManager::GT_ELLIPSOID, 75.0));
 tsafried.getActor().placeIntoScene([-30.4, -6.5, 5.7]);
+
+bot = $DS.loadBot("Tsafried", "tsafried.xml");
+bot.setNonPlayerCharacter(tsafried);
 tsafried.addAction(TalkAction.new("Tsafried", "tsafried.xml"))
+agent = $AI.createAgent(bot);
+agent.addSteeringBehaviour(DefaultWanderBehaviour.new);
+agent.addSteeringBehaviour(AvoidObstaclesBehaviour.new);
+agent.addSteeringBehaviour(DialogBehaviour.new($hero.getActor()));
 
 print( "Tsafried Bot fertig geladen" );
