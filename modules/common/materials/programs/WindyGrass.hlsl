47dbc04aed8895f3ebbe4041d3619b98335c56de
Henrik Hinrichs <henrik@mutaphysis.de>
2006-07-10 12:44:27 +0000

    Bisserl den Shader verändert, auf der Suche nach kaputten Quaternions :)
    git-svn-id: svn://svn.berlios.de/dsa-hl@2338 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/materials/programs/WindyGrass.hlsl
+++ modules/common/materials/programs/WindyGrass.hlsl
@@ -1,10 +1,11 @@
 float4x4 matWorldViewProjection;
+float4x4 matWorld;
 
 float fxTime;
 float fzTime;
 float fMaxMovementFactor;
 
-float4 lightPosition;
+float4 lightDirection;
 float4 lightDiffuse;
 float4 ambientLight;
 float4 materialDiffuse;
@@ -24,17 +25,16 @@ struct VS_OUTPUT
     float4 Color    : COLOR;
 };
 
-#define TWOPI 6.28318531f
-
 VS_OUTPUT vs_main( VS_INPUT Input )
 {
     VS_OUTPUT Output;
 
     // Calculate Offset
     float offset = (1.0-Input.TexCoord.y)*fMaxMovementFactor;
+    float4 worldPos = mul( matWorld, Input.Position);
     // Apply 'random' value for position to disturb 
-    fxTime = -0.5 + (fxTime + abs(sin(Input.Position.x/8+Input.Position.z/4)) )/2.0;
-    fzTime = -0.5 + (fzTime + abs(cos(Input.Position.x/4+Input.Position.z/8)) )/2.0;
+    fxTime = -0.5 + (fxTime + abs(sin(worldPos.x+worldPos.z)) )/2.0;
+    fzTime = -0.5 + (fzTime + abs(cos(worldPos.x+worldPos.z)) )/2.0;
     // Apply Offset
     Input.Position.x += fxTime*offset;
     Input.Position.z += fzTime*offset;
@@ -44,15 +44,10 @@ VS_OUTPUT vs_main( VS_INPUT Input )
     Output.TexCoord = Input.TexCoord;
 
     // Light the vertex
-    float3 lightDir = normalize(lightPosition - Input.Position );
-
-    float4 color = ambientLight * materialAmbient;
-    float NdotL = max(dot(Input.Normal, lightDir), 0.0);
-
-    color += lightDiffuse * materialDiffuse * NdotL;
+    float NdotL = saturate(  dot( normalize( Input.Normal ), normalize(lightDirection ) ) );
+    float4 color = ( ambientLight * materialAmbient ) + ( lightDiffuse * materialDiffuse * NdotL );
     Output.Color = color;
 
     // return
-     
     return( Output );
 }
