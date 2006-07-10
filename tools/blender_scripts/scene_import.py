f6eb172382b3e396d7846100f3a6ec36af50254b
Jochen Schäfer <josch@joschs-robotics.de>
2006-07-10 12:55:27 +0000

    Import-Skript im Umbruch -> funktioniert nicht richtig. TODO: Umstellung auf DOM-Parser
    git-svn-id: svn://svn.berlios.de/dsa-hl@2339 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- tools/blender_scripts/scene_import.py
+++ tools/blender_scripts/scene_import.py
@@ -46,6 +46,8 @@ import re
 import math
 import xml.sax
 import xml.sax.handler
+import xml.dom.minidom
+from xml.dom.minidom import *
 import ogre_import
 import Blender.Mathutils
 from Blender.Mathutils import *
@@ -70,18 +72,32 @@ class Scene:
 class Node:
     def __init__(self):
         self.name = "NoName"
+        self.id = -1
         self.matrix = Matrix()
         self.matrix.resize4x4()
         self.matrix.identity()
-        self.id = -1
-        self.mesh = 0
+        self.object = 0
         
-class NodeMesh(ogre_import.Mesh):
+class NodeObject:
     def __init__(self):
-        ogre_import.Mesh.__init__(self)
+        self.name = "NoName"
+        self.id = -1
+
+class NodeLight(NodeObject):
+    def __init__(self):
+        NodeObject.__init__(self)
+
+class NodeCamera(NodeObject):
+    def __init__(self):
+        NodeObject.__init__(self)
+
+class NodeMesh(NodeObject):
+    def __init__(self):
+        NodeObject.__init__(self)
+        self.mesh = ogre_import.Mesh()
         self.materialfile = 0
         self.filename = 0
-        self.meshid = -1
+
 
 class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
     global IMPORT_SCENE_SCALE_FACTOR
@@ -98,29 +114,32 @@ class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
         
     def startDocument(self):
         self.scene = Scene()
-        self.mesh = NodeMesh()
         self.node = Node()
         self.counter = 1
             
-    def translate(self, m, x, y, z):
-        x *= ogre_import.IMPORT_SCALE_FACTOR
-        y *= ogre_import.IMPORT_SCALE_FACTOR
-        z *= ogre_import.IMPORT_SCALE_FACTOR
+    def translate(self, m, attrs):
+        x = float(attrs.get('x', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR
+        y = -float(attrs.get('z', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR
+        z = float(attrs.get('y', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR 
         m1 = TranslationMatrix(Vector([x, y, z]))
         m *= m1
         return m
 
-    def scale(self, m, x, y, z):
-        x *= ogre_import.IMPORT_SCALE_FACTOR
-        y *= ogre_import.IMPORT_SCALE_FACTOR
-        z *= ogre_import.IMPORT_SCALE_FACTOR
+    def scale(self, m, attrs):
+        x = float(attrs.get('x', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR
+        y = -float(attrs.get('z', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR
+        z = float(attrs.get('y', "0.0")) * ogre_import.IMPORT_SCALE_FACTOR 
         m1 = ScaleMatrix(x, 4, Vector([1.0, 0.0, 0.0]))
         m2 = ScaleMatrix(y, 4, Vector([0.0, 1.0, 0.0]))
         m3 = ScaleMatrix(z, 4, Vector([0.0, 0.0, 1.0]))
         m *= m1 * m2 * m3
         return m
     
-    def rotate(self, m, qx, qy, qz, qw):
+    def rotate(self, m, attrs):
+        qx = float(attrs.get('qx', "0.0"))
+        qy = -float(attrs.get('qz', "0.0"))
+        qz = float(attrs.get('qy', "0.0"))
+        qw = float(attrs.get('qw', "0.0"))
         q = Quaternion([qw, qx, qy, qz])
         qm = Blender.Mathutils.RotationMatrix( \
            q.angle, 4, "r", q.axis)
@@ -128,6 +147,7 @@ class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
         return m
     
     def handleEntity(self, attrs):
+        self.node.object = NodeMesh()
         self.mesh.materialFile = attrs.get('materialFile', "")
 
         meshfilename = attrs.get('meshFile', "")
@@ -139,25 +159,7 @@ class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
             meshfilename  = meshfilename[0 : meshfilename.lower().find( '.mesh.xml' )]
 
         if (self.scene.meshes.has_key(meshfilename) == False):
-            if ( meshfile.lower().find( '.xml' ) == -1 ):
-                xmlname =  meshfile + '.xml'
-                xml_files = glob.glob(xmlname)
-                if (len(xml_files) == 0):
-                    # No. Use the xml converter to fix this
-                    log( "No mesh.xml file. Trying to convert it from binary mesh format." )
-                    ogre_import.convert_meshfile( meshfile )
-                else:
-                    log("%s already exists. No need for conversion." % xmlname)
-                meshfile = xmlname
-                
-            try:
-                dlog("Parsing mesh %s " % meshfile)
-                self.meshparser.parse( meshfile )
-            except Exception, e:
-                log( "    error parsing mesh %s " % \
-                             ( meshfile ) )
-                log( "        exception: %s" % str( e ) )
-            self.mesh = self.meshhandler.mesh
+            self.mesh = parseMesh(meshfile, self.meshparser, self.meshhandler)
             self.mesh.name = attrs.get('name', meshfilename)
                 
             dlog("Meshname %s" % self.mesh.name)
@@ -173,8 +175,16 @@ class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
         else:
             if self.counter < self.node.id:
                 self.counter = self.node.id + 1
-                         
+        self.node.object = NodeMesh()             
             # TODO: Castshadow, userdata
+
+    def handleLight(self, attrs):
+        self.node.object = NodeLight()
+        self.light.name = attrs.get('name', "noname")
+
+    def handleCamera(self, attrs):
+        self.node.object = NodeCamera()
+        self.camera.name = attrs.get('name', "noname")
 
     def startElement(self, name, attrs):
         if name == "node":
@@ -182,59 +192,64 @@ class DotSceneSaxHandler(xml.sax.handler.ContentHandler):
             self.node = Node()
             
         if name == "position":
-            x = float(attrs.get('x', "0.0")) 
-            y = -float(attrs.get('z', "0.0"))
-            z = float(attrs.get('y', "0.0")) 
             self.node.matrix = \
-                self.translate(self.node.matrix, x, y, z) 
+                self.translate(self.node.matrix, attrs) 
             
         if name == "rotation":
-            qx = float(attrs.get('qx', "0.0"))
-            qy = -float(attrs.get('qz', "0.0"))
-            qz = float(attrs.get('qy', "0.0"))
-            qw = float(attrs.get('qw', "0.0"))
             self.node.matrix = \
-                self.rotate(self.node.matrix, \
-                     qx, qy, qz, qw)
+                self.rotate(self.node.matrix, attrs)
             
         if name == "scale":
-            x = float(attrs.get('x', "0.0"))
-            y = float(attrs.get('z', "0.0"))
-            z = float(attrs.get('y', "0.0"))
             self.node.matrix = \
-                self.scale(self.node.matrix, x, y, z)
+                self.scale(self.node.matrix, attrs)
             
         if name == "entity":
             self.handleEntity(attrs)
+            
+        if name == "light":
+            self.handleLight(attrs)
+            
+        if name == "camera":
+            self.handleCamera(attrs)
         
     def endElement(self, name):
         if name == "node":
             dlog("Saving Node ID %d" % self.node.id)
-            self.node.mesh = self.mesh
+            if (self.node.object.name == None):
+                self.node.object.name = self.node.name
+            self.node.object.name = str(self.node.object.name)
+            if str(self.node.object.__class__) == "__main__.NodeMesh":
+                dlog("Remembering the Mesh")
+                self.scene.meshes[self.node.object.name] = self.node.object
             self.scene.nodes.append(self.node)
-            if (self.mesh.name == None):
-                self.mesh.name = self.node.name
-            self.mesh.name = str(self.mesh.name)
-            self.scene.meshes[self.mesh.name] = self.mesh
-      
-def fileselection_callback(filename):
-    log("Reading scene file %s..." % filename)
 
-    dirname = Blender.sys.dirname(filename)
-    basename = Blender.sys.basename(filename)
 
-    # parse material files and make up a dictionary: {mat_name:material, ..}
-    materials = ogre_import.collect_materials(dirname)
+def parseMesh(meshfile, parser, handler):
+    if ( meshfile.lower().find( '.xml' ) == -1 ):
+        xmlname =  meshfile + '.xml'
+        xml_files = glob.glob(xmlname)
+        xmlstat = os.stat(xmlname)
+        meshstat = os.stat(meshfile)
+        if (len(xml_files) == 0) or (xmlstat.st_mtime < meshstat.st_mtime):
+            # No. Use the xml converter to fix this
+            log( "No mesh.xml file. Trying to convert it from binary mesh format." )
+            ogre_import.convert_meshfile( meshfile )
+        else:
+            log("%s already exists. No need for conversion." % xmlname)
+        meshfile = xmlname
+        
+    try:
+        dlog("Parsing mesh %s " % meshfile)
+        parser.parse( meshfile )
+    except Exception, e:
+        log( "    error parsing mesh %s " % \
+                     ( meshfile ) )
+        log( "        exception: %s" % str( e ) )
+        return None
     
-    # Init parser, then parse
-    parser = xml.sax.make_parser()   
-    handler = DotSceneSaxHandler(dirname)
-    parser.setContentHandler(handler)
-    parser.parse(filename)
- 
-    for k, v in handler.scene.meshes.iteritems():
-        dlog("Key %s (%s)" % (k, type(k)))
- 
+    return handler.mesh
+
+def createScene(basename, dotscene):
     # Now we create a new scene
     name = basename[0:basename.lower().find('.scene')]
     log("Creating scene %s..." % name)
@@ -253,30 +268,59 @@ def fileselection_callback(filename):
 #    #scene.link(ob)   
     scene.makeCurrent() 
         
-    nodescount = len(handler.scene.nodes)
+    nodescount = len(dotscene.nodes)
     for i in range(0, nodescount):
-        node = handler.scene.nodes[i]
-        mesh = node.mesh
-        if mesh != None:
-            dlog("Mesh name %s" % mesh.name)
-            bmesh = Blender.NMesh.GetRaw(mesh.name)
-            print(bmesh)
-            if bmesh != None:
-                log("Mesh is already registered")
-            else:
-                log("Importing mesh %s into scene" % (mesh.name))
-                bmesh = ogre_import.CreateBlenderNMesh(str(mesh.name), mesh, materials)
+        node = dotscene.nodes[i]
+        object = node.object
+        if object != None:
+            if str(object.__class__) == "__main__.NodeMesh":
+                log ("Creating Blender Mesh %s " % object.name)
+                bmesh = Blender.NMesh.GetRaw(object.name)
+                print(bmesh)
+                if bmesh != None:
+                    log("Mesh is already registered")
+                else:
+                    log("Importing mesh %s into scene" % (object.name))
+                    bmesh = ogre_import.CreateBlenderNMesh(str(object.name), object.mesh, materials)
                         
-            object = Blender.Object.New( 'Mesh', node.name)
-            object.link(bmesh)
+                bobject = Blender.Object.New( 'Mesh', node.name)
+                bobject.link(bmesh)
                   
-            # apply transformation matrix 
-            object.setMatrix(node.matrix)
-            scene.link(object)
-            object.select(True)
+                # apply transformation matrix 
+                bobject.setMatrix(node.matrix)
+                scene.link(bobject)
+                bobject.select(True)
+                
+            elif str(object.__class__) == "__main__.NodeLight":
+                log ("Creating Blender Light %s " % object.name)
+            
+            elif str(object.__class__) == "__main__.NodeCamera":
+                log ("Creating Blender Camera %s " % object.name)
+
+            else:
+                vlog("Unknown object type")
 
     log ("Scene import done.")
     Blender.Redraw() 
+          
+def fileselection_callback(filename):
+    log("Reading scene file %s..." % filename)
 
+    dirname = Blender.sys.dirname(filename)
+    basename = Blender.sys.basename(filename)
+
+    # parse material files and make up a dictionary: {mat_name:material, ..}
+    materials = ogre_import.collect_materials(dirname)
+    
+    # Init parser, then parse
+    parser = xml.sax.make_parser()   
+    handler = DotSceneSaxHandler(dirname)
+    parser.setContentHandler(handler)
+    parser.parse(filename)
+ 
+    for k, v in handler.scene.meshes.iteritems():
+        dlog("Key %s (%s)" % (k, type(k)))
+ 
+    createScene(basename, handler.scene)
 
 Blender.Window.FileSelector(fileselection_callback, "Import DotScene", "*.scene")
