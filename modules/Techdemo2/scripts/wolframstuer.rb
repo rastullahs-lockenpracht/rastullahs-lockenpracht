abe399d88c402c7e8a0eb8edd4e75c185b494a43
Simon Klein <pnyx1@gmx.net>
2006-07-14 14:54:25 +0000

    Techdemo2 update - Wolframs tür in sein Haus eingebaut + kleiner Raum dahinter wo er sich verstecken kann...
    git-svn-id: svn://svn.berlios.de/dsa-hl@2376 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/wolframstuer.rb
+++ modules/Techdemo2/scripts/wolframstuer.rb
@@ -17,8 +17,8 @@ print( "Wolframs Tür wird geladen" );
 print( "Definiere Listener" );
 
 wolframstuer = Creature.new("WolframsTuer", "WolframsTuer");
-wolframstuer.setActor($AM.createMeshActor("WolframsTuer", "arc_tuer_01.mesh" ));
-wolframstuer.getActor().placeIntoScene([-24.3, -6.6, 5.6]);
+wolframstuer.setActor($AM.createMeshActor("WolframsTuer", "arc_door_02.mesh" ));
+wolframstuer.getActor().placeIntoScene([-20.8134, -6.61745, 1.91752],[0, 13, 0]);#<rotation qx="0.0" qy="0.785045" qz="0.0" qw="0.619439" />
 wolframstuer.addAction(TurTalkAction.new("WolframsTuer", "wolframstuer.xml"))
 
 print( "Wolframs Tür fertig geladen" );
