19f3a1c6c5d00cd386d88a17bc92b669cde78210
Nils Gey <ich@nilsgey.de>
2006-07-14 18:20:36 +0000

    Wolfram weiter reingesetzt
    git-svn-id: svn://svn.berlios.de/dsa-hl@2378 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/wolfram.rb
+++ modules/Techdemo2/scripts/wolfram.rb
@@ -16,6 +16,6 @@ print( "Definiere Listener" );
 
 wolfram = Creature.new("Wolfram", "Wolfram");
 wolfram.setActor($AM.createMeshActor("Wolfram", "men_grim.mesh" ));
-wolfram.getActor().placeIntoScene([-20.9051780700684, -6.61140871047974, 1.40333890914917],[ 0, 190.454895019531,0]);
+wolfram.getActor().placeIntoScene([-22.0, -6.5, -3.0],[ 0, 190.454895019531,0]);
 wolfram.addAction(WolframTalkAction.new("Wolfram", "wolfram.xml"))
 print( "Wolfram Bot fertig geladen" );
