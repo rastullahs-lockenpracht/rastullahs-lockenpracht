914a3b1989e9db1a572c34698d166d6877801427
Henrik Hinrichs <henrik@mutaphysis.de>
2006-07-17 16:27:39 +0000

    Passendener Wüstenshader, bringt ein paar Frames, aber am meisten macht das Anisotropische Filtering aus
    git-svn-id: svn://svn.berlios.de/dsa-hl@2403 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ modules/common/materials/programs/Splatting3NoDetailVertexPhong.hlsl
@@ -0,0 +1,68 @@
+float4 vLightPosition;
+float4 cLightDiffuse;
+float4 cAmbientLight;
+
+float4 cMaterialDiffuse;
+float4 cMaterialAmbient;
+float4x4 matWorldViewProjection;
+
+struct VS_INPUT 
+{
+   float4 Position : POSITION;
+   float2 Texcoord : TEXCOORD0;
+   float3 Normal :   NORMAL;
+};
+
+struct VS_OUTPUT 
+{
+   float4 Position :        POSITION;
+   float2 Texcoord :        TEXCOORD0;
+   float4 Color :           COLOR0;
+};
+
+VS_OUTPUT vs_main( VS_INPUT Input )
+{
+   VS_OUTPUT Output;
+
+   Output.Position         = mul( matWorldViewProjection, Input.Position );
+   Output.Texcoord         = Input.Texcoord;
+
+   // Light the vertex
+   float3 lightDir = normalize(vLightPosition.xyz -  (Input.Position * vLightPosition.w));
+   float NdotL = clamp( dot( normalize( Input.Normal ), lightDir ), 0.0, 1.0);
+   Output.Color = saturate( ( cAmbientLight * cMaterialAmbient ) + ( cLightDiffuse * cMaterialDiffuse * NdotL ) );
+   return( Output );
+}
+
+float scaleBase;
+float scaleBaseR;
+float scaleBaseG;
+
+sampler2D splattingMap;
+sampler2D base;
+sampler2D baseR;
+sampler2D baseG;
+
+struct PS_INPUT 
+{
+   float2 Texcoord :        TEXCOORD0;
+   float4 Color:   COLOR0;
+};
+
+float4 ps_main( PS_INPUT Input ) : COLOR0
+{        
+    float4 fvSplattingColor = tex2D( splattingMap, Input.Texcoord );
+    float4 fvBaseColor      = tex2D( base, Input.Texcoord / scaleBase );
+
+    fvBaseColor            *= 1.0-fvSplattingColor[0]; 
+    fvBaseColor            += ( tex2D(baseR, Input.Texcoord / scaleBaseR ) )
+                              * fvSplattingColor[0];
+                              
+    fvBaseColor            *= 1.0-fvSplattingColor[1]; 
+    fvBaseColor            += ( tex2D(baseG, Input.Texcoord / scaleBaseG ) )
+                              * fvSplattingColor[1];
+
+    return( Input.Color * fvBaseColor );      
+}
+
+
