02889086b852f0ab6fb96178716934ff18d05c5d
Daniel Nasseh <daniel-nasseh@lycos.de>
2006-07-18 13:04:57 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@2417 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/viecher.rb
+++ modules/Techdemo2/scripts/viecher.rb
@@ -5,15 +5,15 @@ print( "Alle Viecher werden geladen" );
 
 schmetterling1 = $AM.createMeshActor("Schmetterling1", "tie_schmetterling_01.mesh" );
 schmetterling1.placeIntoScene(13.54, -5.96, 38.05, 1.0, 0.0, 0.0, 0.0);
-schmetterling1.getControlledObject().startAnimation("Idle");
+schmetterling1.getControlledObject().startAnimation("idle");
 
 schmetterling2 = $AM.createMeshActor("Schmetterling2", "tie_schmetterling_02.mesh" );
 schmetterling2.placeIntoScene(11.82, -6.50, 43.04, 1.0, 0.0, 0.0, 0.0);
-schmetter2Anim = schmetterling2.getControlledObject().startAnimation("Idle");
+schmetter2Anim = schmetterling2.getControlledObject().startAnimation("fliegen");
 schmetter2Anim.setSpeed( 1.25 );
 
 hase = $AM.createMeshActor("Hase", "tie_hase_braun.mesh" );
 hase.placeIntoScene(14.9, -9.1, 42.5, 1.0, 0.0, 0.0, 0.0);
-# Anis defekt!  hase.getControlledObject().startAnimation("walk"); 
+  hase.getControlledObject().startAnimation("idle"); 
 
 print("Alle Viecher fertig geladen");
