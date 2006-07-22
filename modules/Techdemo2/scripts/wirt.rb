d9c2aa43407cb1bb5ea037236e9e8ff19efb31e1
Simon Klein <pnyx1@gmx.net>
2006-07-22 17:22:09 +0000

    Wirt mit anis.
    git-svn-id: svn://svn.berlios.de/dsa-hl@2485 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/wirt.rb
+++ modules/Techdemo2/scripts/wirt.rb
@@ -6,12 +6,12 @@ print( "Wirt wird geladen" );
 
 print( "Definiere Listener" );
 
-wirtActor = $AM.createMeshActor("Wirt", "men_wirt_posed.mesh" );
+wirtActor = $AM.createMeshActor("Wirt", "men_wirt.mesh" );
  
 wirt = Creature.new("Wirt", "Ein Wirt. Er lauscht dem Bauern und lässt sich nicht stören.");
 wirt.setActor( wirtActor );
  
-wirtActor.placeIntoScene(-27.10, -6.48, -1.40, 1.0, 0.0, 0.0, 0.0);
-#wirtActor.getControlledObject().startAnimation("reden");
+wirtActor.placeIntoScene([-27.10, -6.48, -1.40], [0, 180, 0]);
+wirtActor.getControlledObject().startAnimation("reden");
 
 print( "Wirt fertig geladen" );
