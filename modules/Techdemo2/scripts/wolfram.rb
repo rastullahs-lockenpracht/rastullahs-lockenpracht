1ecf83dca1fbbc972a9ede88d60185ef5920a167
Nils Gey <ich@nilsgey.de>
2006-08-02 19:31:38 +0000

    Wolfram debugt und richtig platziert
    git-svn-id: svn://svn.berlios.de/dsa-hl@2573 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Techdemo2/scripts/wolfram.rb
+++ modules/Techdemo2/scripts/wolfram.rb
@@ -2,32 +2,7 @@
 load( "embed.rb" );
 load( "npc.rb" );
 
-class WolframTalkAction < TalkAction
-    def doAction(object, actor, target)
-        if (RulesSubsystem.getSingleton().getQuestBook().getQuest("wolfram").getState() == Quest::SUCCEEDED) 
-            super(object, actor, target)
-	    end
-    end
 
-    def canDo(enemy, user)    
-        RulesSubsystem.getSingleton().getQuestBook().getQuest("wolfram").getState() == Quest::SUCCEEDED
-    end
-end
 
-print( "Wolfram Bot wird geladen" );
+#print( "Wolfram Bot fertig geladen" );
 
-print( "Definiere Listener" );
-
-wolfram = Creature.new("Wolfram", "Wolfram");
-wolfram.setActor($AM.createMeshActor("Wolfram", "men_grim.mesh", PhysicsManager::GT_ELLIPSOID, 75.0));
-wolfram.getActor().placeIntoScene([-22.0, -6.5, -3.0],[ 0, 190.454895019531,0]);
-
-bot = $DS.loadBot("Wolfram", "wolfram.xml");
-bot.setNonPlayerCharacter(wolfram);
-wolfram.addAction(WolframTalkAction.new("Wolfram", "wolfram.xml"))
-agent = $AI.createAgent(bot);
-agent.addSteeringBehaviour(DefaultIdleBehaviour.new);
-#agent.addSteeringBehaviour(AvoidObstaclesBehaviour.new);
-agent.addSteeringBehaviour(DialogBehaviour.new($heroVehicle));
-
-print( "Wolfram Bot fertig geladen" );
--- modules/Techdemo2/scripts/wolframstuer_oeffnen.rb
+++ modules/Techdemo2/scripts/wolframstuer_oeffnen.rb
@@ -3,10 +3,39 @@ load( "embed.rb" );
 require( "npc.rb" );
 require( "door.rb" );
 
+#class WolframTalkAction < TalkAction
+#    def doAction(object, actor, target)
+#        if (RulesSubsystem.getSingleton().getQuestBook().getQuest("wolfram").getState() == Quest::SUCCEEDED) 
+#            super(object, actor, target)
+#	    end
+#    end
+
+#    def canDo(enemy, user)    
+#        RulesSubsystem.getSingleton().getQuestBook().getQuest("wolfram").getState() == Quest::SUCCEEDED
+#    end
+#end
+
+print( "Wolfram Bot wird geladen" );
+
+print( "Definiere Listener" );
+
+wolfram = Creature.new("Wolfram", "Wolfram");
+wolfram.setActor($AM.createMeshActor("Wolfram", "men_grim.mesh", PhysicsManager::GT_ELLIPSOID, 75.0));
+wolfram.getActor().placeIntoScene([-20.99, -6.5, 1.55],[0.0, 180.0, 0.0]);
+
+bot = $DS.loadBot("Wolfram", "wolfram.xml");
+bot.setNonPlayerCharacter(wolfram);
+wolfram.addAction(TalkAction.new("Wolfram", "wolfram.xml"))
+#wolfram.addAction(WolframTalkAction.new("Wolfram", "wolfram.xml"))
+agent = $AI.createAgent(bot);
+agent.addSteeringBehaviour(DefaultIdleBehaviour.new);
+#agent.addSteeringBehaviour(AvoidObstaclesBehaviour.new);
+agent.addSteeringBehaviour(DialogBehaviour.new($heroVehicle));
+
+
+
+
 tuer = $AM.getActor("WolframsTuer")
 tuer.getControlledObject().startAnimation("auf",1.0,1)
 tuer.getControlledObject().replaceAnimation("zu", "auf", 1.0, 1)
 tuer.getPhysicalThing().fitToPose("zu")
-
-wolfram = $AM.getActor("Wolfram")
-wolfram.getControlledObject().startAnimation("gehen",1.0,3)
