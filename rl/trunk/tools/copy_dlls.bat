9384b9a3cdd4a939376b2e3db9e0c0869a0ebbef
Sascha Kolewa <blakharaz@dsa-hl.de>
2006-08-10 09:15:25 +0000

    Fmod4-Treiber und OpenAL-Treiber kopieren
    git-svn-id: svn://svn.berlios.de/dsa-hl@2640 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/tools/copy_dlls.bat
+++ rl/trunk/tools/copy_dlls.bat
@@ -21,3 +21,5 @@ copy "%RL_ROOT%\engine\uicomponents\lib\%BUILD%\RlUiComponents.dll" "%RL_DIST%"
 copy "%RL_ROOT%\engine\ai\lib\%BUILD%\RlAI.dll" "%RL_DIST%"
 
 copy "%RL_ROOT%\plugins\fmod3driver\lib\%BUILD%\RlFmod3Driver.dll" "%RL_DIST%"
+copy "%RL_ROOT%\plugins\fmod4driver\lib\%BUILD%\RlFmod4Driver.dll" "%RL_DIST%"
+copy "%RL_ROOT%\plugins\openaldriver\lib\%BUILD%\RlOpenALDriver.dll" "%RL_DIST%"
