560eee487f73609f94c4b2c76cce50be7c9c011c
Sascha Kolewa <blakharaz@dsa-hl.de>
2006-11-17 17:28:13 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@2859 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/regressiontest/scripts/InventoryTest.rb
+++ modules/regressiontest/scripts/InventoryTest.rb
@@ -5,30 +5,32 @@ class InventoryTest < TestCase
   def initialize(center)
     super(center)
 
-    longsword = $GOM.createGameObjectProxy("shortsword").getGameObject();
-
-    $SCRIPT.log("InventoryTest #1: Put an object a creature's slot.");
     hero = $GOM.getGameObjectProxy(1).getGameObject();
-    hero.getInventory().hold(longsword, "Right Hand");
-
-    $SCRIPT.log("InventoryTest #2: Put an object a creature's slot.");
     backpack = $GOM.createGameObjectProxy("leather backpack").getGameObject();
     backpack.setVolume(5, 5);
+    
+    p "InventoryTest #1: Put an object a creature's slot."
     hero.getInventory().hold(backpack, "Back");
-
-    $SCRIPT.log("InventoryTest #3: Put a loaded object into container.");
+    
+    $SCRIPT.log("InventoryTest #2: Put a loaded object into container.");
     sword_loaded_to_inventory = $GOM.createGameObjectProxy("shortsword").getGameObject();
     hero.getInventory().getItem("Back").addItem(sword_loaded_to_inventory);
 
-    $SCRIPT.log("InventoryTest #4: Put an object from the scene into container.");
+    $SCRIPT.log("InventoryTest #3: Put an object from the scene into container.");
     sword_in_scene_to_inventory = $GOM.createGameObjectProxy("shortsword").getGameObject();
     sword_in_scene_to_inventory.placeIntoScene();
     hero.getInventory().getItem("Back").addItem(sword_in_scene_to_inventory);
 
-    $SCRIPT.log("InventoryTest #5: Put an object attached to the hero into container.");
+    $SCRIPT.log("InventoryTest #4: Put an object attached to the hero into container.");
     sword_held_to_inventory = $GOM.createGameObjectProxy("shortsword").getGameObject();
     hero.getInventory().hold(sword_held_to_inventory, "Left Hand");
     hero.getInventory().getItem("Back").addItem(sword_held_to_inventory);
+
+    $SCRIPT.log("InventoryTest #5: Put an object from a container into the hero's hand.");
+    sword_inventory_to_hold = $GOM.createGameObjectProxy("shortsword").getGameObject();
+    hero.getInventory().getItem("Back").addItem(sword_inventory_to_hold)
+    hero.getInventory().getItem("Back").removeItem(sword_inventory_to_hold)
+    hero.getInventory().hold(sword_inventory_to_hold, "Right Hand");
 
     p "InventoryTest Expectation: Hero should stand with a backpack on her back and a sword attached to the right hand, there should be no sword on the floor or in the left hand";
   end
