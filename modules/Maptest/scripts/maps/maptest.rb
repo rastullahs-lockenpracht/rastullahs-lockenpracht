90dbe3de77af920420407980072ef24fc2b387ae
Simon Klein <pnyx1@gmx.net>
2006-11-29 14:57:42 +0000

    Ruchin WIP
    git-svn-id: svn://svn.berlios.de/dsa-hl@2896 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Maptest/scripts/maps/maptest.rb
+++ modules/Maptest/scripts/maps/maptest.rb
@@ -1,23 +1,28 @@
 load "embed.rb"
-
+$World = $CORE.getWorld()
 require 'player.rb'
 require 'hero.rb'
 
 # Physik aktivieren
 $PM.setEnabled(true);
 
-#$CORE.getWorld().setSkyBox(true, "rl/desert07", 100.0)
+$CORE.getWorld().setSkyBox(true, "rl/dsa07", 100.0)
 
-shadowlight = $AM.createLightActor("shadowlight", LightObject::LT_POINT);
-shadowlight.placeIntoScene([0.0, 4.14, 0.0]);
-shadowlight.getControlledObject().setCastShadows(true);
-shadowlight.getControlledObject().setDiffuseColour(0.8,0.8,0.8);
+$SCRIPT.log("Tageslicht erstellen..");
+sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
+sunlight.getControlledObject().setDirection(-0.17101, -0.873647, 0.969846);
+sunlight.getControlledObject().setCastShadows(true);
+sunlight.getControlledObject().setDiffuseColour(0.8,0.8,0.8);
+$SCRIPT.log("Tageslicht erstellt.");
+
+$World.setFog( World::FOG_EXP, [0.4,0.5,0.6,0.7], 0.00219, 0.5, 1.0);
+#$World.setAmbientLight( 0.2, 0.2, 0.2  );
 
 $SCRIPT.log("Held erstellen");
 $hero = Hero.new;
 $SCRIPT.log("Held erstellt");
 $SCRIPT.log("Held in die Szene einfuegen.");#-22.0130577087402, -6.70670890808105, 25.1438484191895
-$hero.getActor().placeIntoScene([0.0, 0.0, 0.0]);
+$hero.getActor().placeIntoScene([0.0, 20.0, 0.0]);
 $SCRIPT.log("Held eingefügt.");
 PlayerSettings.preparePlayer($hero);
 $SCRIPT.log("Held vorbereitet.");
