8e48fabe7e4197d16fcea0fcef1fc88770f235a2
Simon Klein <pnyx1@gmx.net>
2006-12-13 13:12:27 +0000

    partikel in externe rb ausgelagert.
    git-svn-id: svn://svn.berlios.de/dsa-hl@2918 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/Maptest/scripts/maps/maptest.rb
+++ modules/Maptest/scripts/maps/maptest.rb
@@ -11,7 +11,7 @@ $CORE.getWorld().setSkyBox(true, "rl/dsa07", 100.0)
 $SCRIPT.log("Tageslicht erstellen..");
 sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
 sunlight.getControlledObject().setDirection(-0.17101, -0.873647, 0.969846);
-sunlight.getControlledObject().setCastShadows(true);
+sunlight.getControlledObject().setCastShadows(false);
 
 sunlight.getControlledObject().setDiffuseColour(1.0,1.0,1.0);
 #sunlight.getControlledObject().setDiffuseColour(0.1,0.1,0.2);
@@ -20,27 +20,11 @@ $SCRIPT.log("Tageslicht erstellt.");
 
 backlight = $AM.createLightActor("backlight", LightObject::LT_DIRECTIONAL);
 backlight.getControlledObject().setDirection(0.17101, 0.873647, -0.969846);
-backlight.getControlledObject().setCastShadows(true);
+backlight.getControlledObject().setCastShadows(false);
 backlight.getControlledObject().setDiffuseColour(-0.3,-0.3,-0.3);
 
 
-tavernfire1 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
-tavernfire1.placeIntoScene( 94.28, 5.65, -30.88, 1.0, 0.0, 0.0, 0.0 );
 
-qualm1 = $AM.createParticleSystemActor("qualm1", "qualm" );
-qualm1.placeIntoScene( -4.28, 3.00, -1.66, 1.0, 0.0, 0.0, 0.0 );
-
-qualm2 = $AM.createParticleSystemActor("qualm2", "qualm" );
-qualm2.placeIntoScene( -3.10, 2.18, 6.12, 1.0, 0.0, 0.0, 0.0 );
-
-qualm3 = $AM.createParticleSystemActor("qualm3", "qualm" );
-qualm3.placeIntoScene( 34.31, 10.60, -4.16, 1.0, 0.0, 0.0, 0.0 );
-
-qualm4 = $AM.createParticleSystemActor("qualm4", "qualm" );
-qualm4.placeIntoScene( 0.42, 3.78, 22.26, 1.0, 0.0, 0.0, 0.0 );
-
-qualm5 = $AM.createParticleSystemActor("qualm5", "qualm" );
-qualm5.placeIntoScene( 92.39, 18.35, -30.55, 1.0, 0.0, 0.0, 0.0 );
 
 
 $World.setFog( World::FOG_EXP, [0.4,0.5,0.6,0.7], 0.00219, 0.5, 1.0);
@@ -60,6 +44,7 @@ $heroVehicle = $AI.createAgent(AgentManager::AGENT_PLAYER, $hero);
 $SCRIPT.log("SteeringVehicle für Held erstellen...");
 
 
+
 $SCRIPT.log("Dorf-Thema laden");
 testmaptrack = $AM.createSoundSampleActor( "testmaptrack", "testmaptrack.ogg" ); 
 testmaptrack.getControlledObject().set3d(false);
@@ -70,3 +55,6 @@ testmaptrack.getControlledObject().load();
 $SCRIPT.log("Dorf-Thema fertig");
 
 testmaptrack.getControlledObject().play();
+
+
+load "particle.rb"
---
+++ modules/Maptest/scripts/particle.rb
@@ -0,0 +1,34 @@
+
+
+tavernfire1 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire1.placeIntoScene( 94.28, 5.65, -30.88, 1.0, 0.0, 0.0, 0.0 );
+
+#firelight = $AM.createLightActor("firelight", LightObject::LT_POINT);
+#firelight.placeIntoScene( 94.28, 5.65, -30.88, 1.0, 0.0, 0.0, 0.0 );
+#firelight.getControlledObject().setCastShadows(false);
+
+qualm1 = $AM.createParticleSystemActor("qualm1", "qualm" );
+qualm1.placeIntoScene( -4.28, 3.00, -1.66, 1.0, 0.0, 0.0, 0.0 );
+
+qualm2 = $AM.createParticleSystemActor("qualm2", "qualm" );
+qualm2.placeIntoScene( -3.10, 2.18, 6.12, 1.0, 0.0, 0.0, 0.0 );
+
+qualm3 = $AM.createParticleSystemActor("qualm3", "qualm" );
+qualm3.placeIntoScene( 34.31, 10.60, -4.16, 1.0, 0.0, 0.0, 0.0 );
+
+qualm4 = $AM.createParticleSystemActor("qualm4", "qualm" );
+qualm4.placeIntoScene( 0.42, 3.78, 22.26, 1.0, 0.0, 0.0, 0.0 );
+
+qualm5 = $AM.createParticleSystemActor("qualm5", "qualm" );
+qualm5.placeIntoScene( 92.39, 18.35, -30.55, 1.0, 0.0, 0.0, 0.0 );
+
+
+
+#rain = $AM.createParticleSystemActor("rain", "rain" );
+#rain.placeIntoScene( 0.39, 0.35, 0.55, 1.0, 0.0, 0.0, 0.0 );
+
+
+#halter= $hero.getActor();
+#halter.attachToSlotAxisRot( rain, "l_oberarm", "SLOT_HANDLE", [0.0, 0.0, 0.0], [ 0.0, 0.0, 0.0 ], 0.0 );
+
+#halter.attachToSlotAxisRot( firelight, "l_oberarm", "SLOT_HANDLE", [0.0, 0.0, 0.0], [ 0.0, 0.0, 0.0 ], 0.0 );
