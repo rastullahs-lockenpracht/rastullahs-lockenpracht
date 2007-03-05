5440f75a32c5b64ef180e281f2b3adc7b0324b5c
Daniel Wickert <haffax@team-pantheon.de>
2007-03-05 22:56:36 +0000

    Added new combattest module. Doesn't do much yet.
    git-svn-id: svn://svn.berlios.de/dsa-hl@3126 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ modules/combattest/scripts/maps/combattest.rb
@@ -0,0 +1,27 @@
+require 'player.rb'
+require 'hero.rb'
+
+# Physik aktivieren
+$PM.setEnabled(true)
+
+$SCRIPT.log("init map 'regressiontest'...")
+
+$SCRIPT.log("Create hero")
+$hero = $GOM.createGameObjectProxy("hero", 1);
+
+$SCRIPT.log("Place hero into scene")
+$hero.getGameObject().placeIntoScene()
+
+$SCRIPT.log("Prepare hero for being the active character")
+PlayerSettings.preparePlayer($hero.getGameObject())
+
+$SCRIPT.log("Set hero as active character")
+$UI.setActiveCharacter($hero.getGameObject())
+$SCRIPT.log("Hero ready")
+
+wolf = $GOM.createGameObjectProxy("Wolf").getGameObject()
+wolf.placeIntoScene
+wolf.setPosition [0.0,0.0,-5.0]
+
+$SCRIPT.log("map 'combattest' initialisiert.")
+
