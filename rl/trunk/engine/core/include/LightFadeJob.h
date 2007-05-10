edbf34478fbee44de8c8743a5e29cb533a714f8b
Jörg Reichardt <no_22@web.de>
2007-05-10 12:25:34 +0000

    *added missing files
    git-svn-id: svn://svn.berlios.de/dsa-hl@3423 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ rl/trunk/engine/core/include/LightFadeJob.h
@@ -0,0 +1,57 @@
+/* This source file is part of Rastullahs Lockenpracht.
+* Copyright (C) 2003-2007 Team Pantheon. http://www.team-pantheon.de
+* 
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the Clarified Artistic License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  Clarified Artistic License for more details.
+*
+*  You should have received a copy of the Clarified Artistic License
+*  along with this program; if not you can get it here
+*  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+*/
+
+#ifndef __RL_LIGHT_FADE_JOB_H__
+#define __RL_LIGHT_FADE_JOB_H__
+
+#include "CorePrerequisites.h"
+#include <OgreSingleton.h>
+#include "LightObject.h"
+
+#include "Job.h"
+
+namespace rl {
+
+    class _RlCoreExport LightFadeJob : public Job
+    {
+    public:
+        LightFadeJob(LightObject* lightObject, const Ogre::ColourValue& targetDiffuse, 
+            const Ogre::ColourValue& targetSpecular, Ogre::Real duration = 1.0f);
+
+        virtual bool execute(Ogre::Real time);
+        virtual void discard();
+
+    protected:
+        /**
+         *  Interpolates between two color values. The interpolated colour is returned in
+         *  currentColour and depends on mTime and mDuration.
+         **/
+        bool interpolateColour(const Ogre::ColourValue& startColour, 
+            const Ogre::ColourValue& endColour, Ogre::ColourValue& currentColour);
+        int mSteps;
+        LightObject* mLightObject;
+        /// The time passed since job start.
+        Ogre::Real mTime;
+        /// The total duration of the fade job.
+        Ogre::Real mDuration;
+        Ogre::ColourValue mTargetDiffuse;
+        Ogre::ColourValue mStartDiffuse;
+        Ogre::ColourValue mTargetSpecular;
+        Ogre::ColourValue mStartSpecular;
+    };
+}
+
+#endif
---
+++ rl/trunk/engine/core/src/LightFadeJob.cpp
@@ -0,0 +1,101 @@
+/* This source file is part of Rastullahs Lockenpracht.
+* Copyright (C) 2003-2007 Team Pantheon. http://www.team-pantheon.de
+* 
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the Clarified Artistic License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  Clarified Artistic License for more details.
+*
+*  You should have received a copy of the Clarified Artistic License
+*  along with this program; if not you can get it here
+*  http://www.jpaulmorrison.com/fbp/artistic2.htm.
+*/
+
+#include "LightFadeJob.h"
+
+
+namespace rl
+{
+    LightFadeJob::LightFadeJob(LightObject* lightObject, const Ogre::ColourValue& targetDiffuse, 
+        const Ogre::ColourValue& targetSpecular, Ogre::Real duration)
+        : Job(true, true),
+        mLightObject(lightObject),
+        mDuration(duration)
+    {
+        if (mLightObject != NULL)
+        {
+            mStartDiffuse = mLightObject->getDiffuseColour();
+            mStartSpecular = mLightObject->getSpecularColour();
+        }
+    }
+
+    bool LightFadeJob::execute(Ogre::Real time)
+    {
+        // Precondition: mLightObject != NULL
+        if (mLightObject == NULL)
+        {
+            discard();
+            return true;
+        }
+
+        // add passed time to total time
+        mTime += time;
+
+        bool diffuseInterpolationFinished = false;
+        bool specularInterpolationFinished = false;
+
+        // interpolate colour values and set light source
+        Ogre::ColourValue currentDiffuse;
+        diffuseInterpolationFinished = interpolateColour(mStartDiffuse, mTargetDiffuse, currentDiffuse);
+        mLightObject->setDiffuseColour(currentDiffuse);
+        Ogre::ColourValue currentSpecular;
+        specularInterpolationFinished = interpolateColour(mStartSpecular, mTargetSpecular, currentSpecular);
+        mLightObject->setSpecularColour(currentSpecular);
+
+        // Are we done now?
+        if (diffuseInterpolationFinished && specularInterpolationFinished)
+        {
+            discard();
+            return true;
+        }
+        //mWindow->getWindow()->setAlpha(mCurrentAlpha);
+
+        return false;
+    }
+
+    void LightFadeJob::discard()
+    {
+        /// @todo check if light source is invisible?
+    }
+
+    bool LightFadeJob::interpolateColour(const Ogre::ColourValue& startColour, 
+        const Ogre::ColourValue& endColour, Ogre::ColourValue& currentColour)
+    {
+        // Postcondition: mTime <= mDuration
+        if (mDuration < mTime)
+            return true;
+
+        // calculate current factor
+        float factor;
+        if (mTime == mDuration)
+        {
+            factor = 1.0f;
+        }
+        else
+        {
+            factor = (float)mTime / (float)mDuration;
+        }
+
+        // vresult = (1-factor)*vstart + factor*vend
+        currentColour = (1.0f - factor) * startColour + factor * endColour;
+
+        // if (finished) return true
+        if (mTime == mDuration)
+            return true;
+        else
+            return false;
+    }
+}
