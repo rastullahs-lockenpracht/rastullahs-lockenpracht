f7e8c1728835264118c3d1690b16ff58ecc57809
Sascha Kolewa <blakharaz@dsa-hl.de>
2007-05-19 10:09:51 +0000

    Adapt to renamed models and animation
    git-svn-id: svn://svn.berlios.de/dsa-hl@3457 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/regressiontest/scripts/MergeableMeshTest.rb
+++ modules/regressiontest/scripts/MergeableMeshTest.rb
@@ -14,7 +14,7 @@ class MergeableMeshTest < TestCase
         test_obj2 = $GOM.createGameObject("mesh construction kit test creature")
         test_obj2.setPosition(rel_pos([1, 0, 0]))
         test_obj2.placeIntoScene();
-#        test_obj2.getActor().getControlledObject().startAnimation("walk")
+#        test_obj2.getActor().getControlledObject().startAnimation("Walk")
         test_obj2.getInventory().addSlot("Head", "head", Item::ITEMTYPE_HELMET, false);
         test_obj2.getInventory().addSlot("Torso", "torso", Item::ITEMTYPE_ALL_ITEMS, false);
         test_item2 = $GOM.createGameObject("mesh construction kit test item 1")
@@ -27,7 +27,7 @@ class MergeableMeshTest < TestCase
         test_obj3.getInventory().addSlot("Torso", "torso", Item::ITEMTYPE_ALL_ITEMS, false);
         test_item3 = $GOM.createGameObject("mesh construction kit test item 2")
         test_obj3.getInventory().hold(test_item3, "Torso")
-        test_obj3.getActor().getControlledObject().startAnimation("walk")
+        test_obj3.getActor().getControlledObject().startAnimation("Walk")
 
         test_obj4 = $GOM.createGameObject("mesh construction kit test creature")
         test_obj4.setPosition(rel_pos([3, 0, 0]))
