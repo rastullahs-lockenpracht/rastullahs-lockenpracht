33fec783fd83e1373b206337d206300ac2190268
Sascha Kolewa <blakharaz@dsa-hl.de>
2007-06-05 22:10:01 +0000

    Move Alrike's properties from her Ruby class to the GOF file
    git-svn-id: svn://svn.berlios.de/dsa-hl@3506 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/common/scripts/hero.rb
+++ modules/common/scripts/hero.rb
@@ -4,18 +4,7 @@ class Hero < Person
     def initialize(id)
         super(id);
         addQueryFlag(RlScript::QUERYFLAG_PLAYER)
-		    setName("Alrike");
-		    setDescription("Heldin von Berufung");
-		    $SCRIPT.log("Gute Eigenschaften zuweisen...");
-
-        setEigenschaft("MU", 12);
-        setEigenschaft("KL", 8);
-        setEigenschaft("IN", 9);
-        setEigenschaft("CH", 10);
-        setEigenschaft("FF", 11);
-        setEigenschaft("GE", 12);
-        setEigenschaft("KO", 13); #KO
-        setEigenschaft("KK", 14); #KK
+		    
 		setWert(Creature::WERT_MOD_LE, 10); # LE-Mod
 
 		$SCRIPT.log("LeP und AuP auffüllen");
@@ -23,15 +12,6 @@ class Hero < Person
         modifyAu(1000); # Au = voll
         #modifyLe(-20); # 10 LeP abziehen
 
-		$SCRIPT.log("Talente hinzufügen");
-		addTalent("Athletik", 3);
-		#addTalent("Körperbeherrschung", 3);
-
-        $SCRIPT.log("Heldenaktor beschreiben...");
-        setProperty("meshfile", "men_alrike.mesh")
-		setProperty("physicsbody", PhysicsManager::GT_ELLIPSOID)
-		setProperty("mass", 75.0);
-	
 		$SCRIPT.log("Inventar konfigurieren.");
 		inv = getInventory();
 		inv.addSlot("Left Ring", "l_finger_2", Item::ITEMTYPE_RING);
