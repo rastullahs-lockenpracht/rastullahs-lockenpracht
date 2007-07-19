ed33f39938c8c70a1d03f3fe25d7c0be7fe32e06
Sascha Kolewa <blakharaz@dsa-hl.de>
2007-07-19 10:18:03 +0000

    Fix null pointer assert exception when no fog is set in scene file
    git-svn-id: svn://svn.berlios.de/dsa-hl@3619 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/script/src/EnvironmentProcessor.cpp
+++ rl/trunk/engine/script/src/EnvironmentProcessor.cpp
@@ -29,141 +29,139 @@ namespace rl
 {
 	bool EnvironmentProcessor::processNode(XERCES_CPP_NAMESPACE::DOMElement* nodeElem, bool loadGameObjects)
 	{
-		if (nodeElem == NULL)
+		if (nodeElem)
 		{
-			return false;
-		}
-
-		processSkySettings(XmlHelper::getChildNamed(nodeElem, "sky"));
-		processFogSettings(XmlHelper::getChildNamed(nodeElem, "fog"));
+		    processSkySettings(XmlHelper::getChildNamed(nodeElem, "sky"));
+		    processFogSettings(XmlHelper::getChildNamed(nodeElem, "fog"));
+        }
 
 		return true;
 	}
 
-
 	void EnvironmentProcessor::processSkySettings(XERCES_CPP_NAMESPACE::DOMElement* skyElem)
 	{
-        if (skyElem == NULL)
+        if (skyElem)
 		{
-			return;
-		}
 
-		if (!XmlHelper::hasAttribute(skyElem, "material")
-			|| !XmlHelper::hasAttribute(skyElem, "type"))
-		{
-			LOG_ERROR(Logger::RULES, "<sky> element must have at least attributes 'type' and 'material'.");
-		}
-		else
-		{
-			Ogre::String type = XmlHelper::getAttributeValueAsStdString(skyElem, "type");
-			Ogre::String material = XmlHelper::getAttributeValueAsStdString(skyElem, "material");
+		    if (!XmlHelper::hasAttribute(skyElem, "material")
+			    || !XmlHelper::hasAttribute(skyElem, "type"))
+		    {
+			    LOG_ERROR(Logger::RULES, "<sky> element must have at least attributes 'type' and 'material'.");
+		    }
+		    else
+		    {
+			    Ogre::String type = XmlHelper::getAttributeValueAsStdString(skyElem, "type");
+			    Ogre::String material = XmlHelper::getAttributeValueAsStdString(skyElem, "material");
 
-			bool drawFirst = true;
-			if (XmlHelper::hasAttribute(skyElem, "drawfirst"))
-			{
-				drawFirst = XmlHelper::getAttributeValueAsBool(skyElem, "drawfirst");
-			}
+			    bool drawFirst = true;
+			    if (XmlHelper::hasAttribute(skyElem, "drawfirst"))
+			    {
+				    drawFirst = XmlHelper::getAttributeValueAsBool(skyElem, "drawfirst");
+			    }
 
-			Ogre::Real distance = 5000;
-			if (XmlHelper::hasAttribute(skyElem, "distance"))
-			{
-				distance = XmlHelper::getAttributeValueAsReal(skyElem, "distance");
-			}
+			    Ogre::Real distance = 5000;
+			    if (XmlHelper::hasAttribute(skyElem, "distance"))
+			    {
+				    distance = XmlHelper::getAttributeValueAsReal(skyElem, "distance");
+			    }
 
-			if (type == "dome")
-			{
-				Ogre::Real curvature = 10;
-				Ogre::Real tiling = 8;
+			    if (type == "dome")
+			    {
+				    Ogre::Real curvature = 10;
+				    Ogre::Real tiling = 8;
 
-				DOMElement* domeSettings = XmlHelper::getChildNamed(skyElem, "skydomesettings");
-				if (domeSettings != NULL)
-				{
-					if (XmlHelper::hasAttribute(domeSettings, "curvature"))
-					{
-						curvature = XmlHelper::getAttributeValueAsReal(domeSettings, "curvature");
-					}
-					if (XmlHelper::hasAttribute(domeSettings, "tiling"))
-					{
-						curvature = XmlHelper::getAttributeValueAsReal(domeSettings, "tiling");
-					}
-				}
-				CoreSubsystem::getSingleton().getWorld()->setSkyDome(
-					true, material, curvature, tiling, distance, drawFirst);
-			}
-			else if (type == "box")
-			{
-				CoreSubsystem::getSingleton().getWorld()->setSkyBox(true, material, distance, drawFirst);
-			}
-			else if (type == "plane")
-			{
-				LOG_ERROR(Logger::RULES, "Sky Plane is not implemented yet.");
-			}
-		}
+				    DOMElement* domeSettings = XmlHelper::getChildNamed(skyElem, "skydomesettings");
+				    if (domeSettings != NULL)
+				    {
+					    if (XmlHelper::hasAttribute(domeSettings, "curvature"))
+					    {
+						    curvature = XmlHelper::getAttributeValueAsReal(domeSettings, "curvature");
+					    }
+					    if (XmlHelper::hasAttribute(domeSettings, "tiling"))
+					    {
+						    curvature = XmlHelper::getAttributeValueAsReal(domeSettings, "tiling");
+					    }
+				    }
+				    CoreSubsystem::getSingleton().getWorld()->setSkyDome(
+					    true, material, curvature, tiling, distance, drawFirst);
+			    }
+			    else if (type == "box")
+			    {
+				    CoreSubsystem::getSingleton().getWorld()->setSkyBox(true, material, distance, drawFirst);
+			    }
+			    else if (type == "plane")
+			    {
+				    LOG_ERROR(Logger::RULES, "Sky Plane is not implemented yet.");
+			    }
+		    }
+        }
 	}
 
 
 	void EnvironmentProcessor::processFogSettings(XERCES_CPP_NAMESPACE::DOMElement* fogElem)
 	{
-		///@todo process fog node
-		if (!XmlHelper::hasAttribute(fogElem, "material"))
-		{
-			LOG_ERROR(
-				Logger::RULES,
-				"<fog> element must have at least the attribute 'type'.");
-			return;
-		}
+        if (fogElem)
+        {
+		    if (!XmlHelper::hasAttribute(fogElem, "material"))
+		    {
+			    LOG_ERROR(
+				    Logger::RULES,
+				    "<fog> element must have at least the attribute 'type'.");
+			    return;
+		    }
 
-		DOMElement* colourElem = XmlHelper::getChildNamed(fogElem, "colour");
-		if (colourElem == NULL)
-		{
-			LOG_ERROR(Logger::RULES, "No fog colour set.");
-			return;
-		}
-		ColourValue fogColour = processColour(colourElem);
+		    DOMElement* colourElem = XmlHelper::getChildNamed(fogElem, "colour");
+		    if (colourElem == NULL)
+		    {
+			    LOG_ERROR(Logger::RULES, "No fog colour set.");
+			    return;
+		    }
+		    ColourValue fogColour = processColour(colourElem);
 
-		Ogre::String type = XmlHelper::getAttributeValueAsStdString(fogElem, "type");
-		if (type == "exp" || type == "exp2")
-		{
-			if (XmlHelper::hasAttribute(fogElem, "density"))
-			{
-				Ogre::Real density = XmlHelper::getAttributeValueAsReal(fogElem, "density");
-				if (type == "exp")
-				{
-					CoreSubsystem::getSingleton().getWorld()->setFog(
-						World::FOG_EXP, fogColour, density);
-				}
-				else if (type == "exp2")
-				{
-					CoreSubsystem::getSingleton().getWorld()->setFog(
-						World::FOG_EXP2, fogColour, density);
-				}
-			}
-			else
-			{
-				LOG_ERROR(
-					Logger::RULES, type + " fog needs attribute 'density'.");
-			}
-		}
-		else if (type == "linear")
-		{
-			if (XmlHelper::hasAttribute(fogElem, "start")
-				&& XmlHelper::hasAttribute(fogElem, "end"))
-			{
-				Ogre::Real start = XmlHelper::getAttributeValueAsReal(fogElem, "start");
-				Ogre::Real end = XmlHelper::getAttributeValueAsReal(fogElem, "end");
-				CoreSubsystem::getSingleton().getWorld()->setFog(
-					World::FOG_LINEAR, fogColour, 0, start, end);
-			}
-			else
-			{
-				LOG_ERROR(
-					Logger::RULES, "linear fog needs attributes 'start' and 'end'.");
-			}
-		}
-		else
-		{
-			LOG_ERROR(
-				Logger::RULES, type + " is an unknown fog type.");
-		}
+		    Ogre::String type = XmlHelper::getAttributeValueAsStdString(fogElem, "type");
+		    if (type == "exp" || type == "exp2")
+		    {
+			    if (XmlHelper::hasAttribute(fogElem, "density"))
+			    {
+				    Ogre::Real density = XmlHelper::getAttributeValueAsReal(fogElem, "density");
+				    if (type == "exp")
+				    {
+					    CoreSubsystem::getSingleton().getWorld()->setFog(
+						    World::FOG_EXP, fogColour, density);
+				    }
+				    else if (type == "exp2")
+				    {
+					    CoreSubsystem::getSingleton().getWorld()->setFog(
+						    World::FOG_EXP2, fogColour, density);
+				    }
+			    }
+			    else
+			    {
+				    LOG_ERROR(
+					    Logger::RULES, type + " fog needs attribute 'density'.");
+			    }
+		    }
+		    else if (type == "linear")
+		    {
+			    if (XmlHelper::hasAttribute(fogElem, "start")
+				    && XmlHelper::hasAttribute(fogElem, "end"))
+			    {
+				    Ogre::Real start = XmlHelper::getAttributeValueAsReal(fogElem, "start");
+				    Ogre::Real end = XmlHelper::getAttributeValueAsReal(fogElem, "end");
+				    CoreSubsystem::getSingleton().getWorld()->setFog(
+					    World::FOG_LINEAR, fogColour, 0, start, end);
+			    }
+			    else
+			    {
+				    LOG_ERROR(
+					    Logger::RULES, "linear fog needs attributes 'start' and 'end'.");
+			    }
+		    }
+		    else
+		    {
+			    LOG_ERROR(
+				    Logger::RULES, type + " is an unknown fog type.");
+		    }
+        }
 	}
 }
