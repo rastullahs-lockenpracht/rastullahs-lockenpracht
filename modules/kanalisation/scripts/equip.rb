3ae47d1e2eaf0777e38b5476ffb6f2af979f3efa
Simon Hartmann <synter@wsfclan.de>
2007-10-11 19:26:50 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@3897 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ modules/kanalisation/scripts/equip.rb
@@ -0,0 +1,23 @@
+require 'npc.rb'
+	
+	$SCRIPT.log("aktiven hero geten");
+	hero = $UI.getActiveCharacter();
+	
+	$SCRIPT.log("backpack erstellen");
+    #backpack = $GOM.createGameObject("leather backpack");
+    #backpack.addAction(ShowContainerContentAction.new());
+    #backpack.setPosition(center);
+    #backpack.placeIntoScene();
+    
+    #$SCRIPT.log("inventar füllen!!!");
+    #hero.getInventory().hold(backpack, "Back");
+    
+    itemToAdd = $GOM.createGameObject("men_human_female_leg_pants_01_long");
+    hero.getInventory().hold(itemToAdd, "Trousers");
+    #hero.getInventory().getItem("Back").addItem(itemToAdd);
+    itemToAdd = $GOM.createGameObject("men_human_female_cloth_short");
+    hero.getInventory().hold(itemToAdd, "Clothes");
+    #hero.getInventory().getItem("Back").addItem(itemToAdd);
+    itemToAdd = $GOM.createGameObject("men_human_female_feet_boots_01");
+    hero.getInventory().hold(itemToAdd, "Boots");
+   # hero.getInventory().getItem("Back").addItem(itemToAdd);
