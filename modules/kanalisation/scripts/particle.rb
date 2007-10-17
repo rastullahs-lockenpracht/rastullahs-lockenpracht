3f5da3bba25cd54757738bfbafff0b3a66e1bb60
Simon Hartmann <synter@wsfclan.de>
2007-10-17 20:47:39 +0000

    git-svn-id: svn://svn.berlios.de/dsa-hl@3932 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/kanalisation/scripts/particle.rb
+++ modules/kanalisation/scripts/particle.rb
@@ -3,6 +3,21 @@
 tavernfire1 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
 tavernfire1.placeIntoScene( 22.44,6.75,8.00, 1.0, 0.0, 0.0, 0.0 );
 
+tavernfire2 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire2.placeIntoScene( 22.44,6.75,15.80, 1.0, 0.0, 0.0, 0.0 );
+
+tavernfire3 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire3.placeIntoScene( -9.34,6.57,19.10, 1.0, 0.0, 0.0, 0.0 );
+
+tavernfire4 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire4.placeIntoScene( -9.14,6.53,2.80, 1.0, 0.0, 0.0, 0.0 );
+
+tavernfire5 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire5.placeIntoScene( -9.14,6.53,-4.74, 1.0, 0.0, 0.0, 0.0 );
+
+tavernfire6 = $AM.createParticleSystemActor("tavernfire1", "feuertest" );
+tavernfire6.placeIntoScene( 22.53,6.70,25.60, 1.0, 0.0, 0.0, 0.0 );
+
 #firelight = $AM.createLightActor("firelight", LightObject::LT_POINT);
 #firelight.placeIntoScene( 94.28, 5.65, -30.88, 1.0, 0.0, 0.0, 0.0 );
 #firelight.getControlledObject().setCastShadows(false);
