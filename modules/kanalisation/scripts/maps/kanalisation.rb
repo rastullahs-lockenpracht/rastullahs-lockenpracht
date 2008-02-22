73dca883065e7501d649f454e65203f52c8f352e
Simon Hartmann <synter@wsfclan.de>
2008-02-22 15:37:29 +0000

    bugged version - messed up materials
    git-svn-id: svn://svn.berlios.de/dsa-hl@4077 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/kanalisation/scripts/maps/kanalisation.rb
+++ modules/kanalisation/scripts/maps/kanalisation.rb
@@ -3,8 +3,7 @@ $World = $CORE.getWorld()
 require 'player.rb'
 require 'hero.rb'
 require 'mckhero.rb'
-require 'door.rb'
-require 'torch.rb'
+#require 'torch.rb'
 
 
 loader = MapLoader.new("kanalisation");
@@ -13,23 +12,18 @@ loader.loadMap("kana_lights.rlmap.xml");
 # Physik aktivieren
 $PM.setEnabled(true);
 
-$SCRIPT.log("rauschendesWasser geladen");
 
-#$hero.getActor().placeIntoScene([0.0, 0.0, 0.0]);
 #$SCRIPT.log("Tageslicht erstellen..");
 #sunlight = $AM.createLightActor("sunlight", LightObject::LT_DIRECTIONAL);
-#$hero.getActor().placeIntoScene([0.0, 0.0, 0.0]);
 #sunlight.getControlledObject().setCastShadows(true);
 #sunlight.getControlledObject().setSpecularColour(1.0,1.0,1.0);
 #sunlight.getControlledObject().setDiffuseColour(0.1,0.1,0.2);
 #$SCRIPT.log("Tageslicht erstellt.");
 
-
-backlight = $AM.createLightActor("backlight", LightObject::LT_DIRECTIONAL);
-backlight.getControlledObject().setDirection(0.17101, 0.873647, -0.969846);
-backlight.getControlledObject().setCastShadows(false);
-backlight.getControlledObject().setDiffuseColour(-0.3,-0.3,-0.3);
-
+#backlight = $AM.createLightActor("backlight", LightObject::LT_DIRECTIONAL);
+#backlight.getControlledObject().setDirection(0.17101, 0.873647, -0.969846);
+#backlight.getControlledObject().setCastShadows(false);
+#backlight.getControlledObject().setDiffuseColour(-0.3,-0.3,-0.3);
 
 $World.setFog( World::FOG_EXP, [0.658,0.7568,0.9137,1.0], 0.00049, 0.8, 1.0);
 $World.setAmbientLight( 0.08, 0.06, 0.05  );
@@ -60,9 +54,6 @@ $UI.setActiveCharacter($hero)
 
 $SCRIPT.log("Hero ready")
 
-#$SCRIPT.log("Held in die Szene einfuegen.");#-22.0130577087402, -6.70670890808105, 25.1438484191895
-#$hero.getActor().placeIntoScene([0.0, 20.0, 0.0]);
-
 #$SCRIPT.log("Fackellicht erstellen..");
 #fackellicht = $AM.createLightActor("Das Licht der Fackel", LightObject::LT_POINT );
 #fackellicht.getControlledObject().setCastShadows(false);
@@ -70,28 +61,6 @@ $SCRIPT.log("Hero ready")
 #fackellicht.getControlledObject().setAttenuation(2.0, 0.79,  -0.21, 0.268 );
 #$hero.getActor().attachToSlot( fackellicht, "Bip01 R SlotHand" );
 #$SCRIPT.log("Fackellicht erstellt.");
-
-
-$SCRIPT.log("Türen einsetzen");
-#door1 = Door.new("Haustuer", "Eine Holztür", [-12.9124, -6.86492, 13.7133], [0, -120, 0], "arc_door_02.mesh");
-#door1.addActions(false, true)
-
-#door2 = Door.new("Tavernentür", "Die Eingangstür zur Taverne", [52.461, 1.28902, -26.323], [-0.462671,0.0, 0.88653, 0.0], "arc_door_tavern.mesh");
-#door2.addActions(false, true)
-
-$SCRIPT.log("Türen gesetzt");
-
-#####################
-
-#BoxActor = $AM.createMeshActor("Box", "facial.mesh" );
-#$SCRIPT.log("actor erstellt");
- 
-#BoxActor.placeIntoScene(0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0);
-#$SCRIPT.log("actor in scene gesetzt");
-#BoxAnim = BoxActor.getControlledObject().startAnimation("Speak");
-#$SCRIPT.log("animation abgespielt.");
-
-##########################
 
 load "particle.rb"
 load "equip.rb"
