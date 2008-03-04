40b1b1f4a45a71f411767f4dd0179de6ba1904a1
Timm Eversmeyer <Saeugetier@gmx.net>
2008-03-04 15:45:57 +0000

    minidemo and tutorial are broken (not needed anymore)
    git-svn-id: svn://svn.berlios.de/dsa-hl@4121 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/minidemo/scripts/sound.rb
+++
@@ -1,7 +0,0 @@
-# Nur eingebettet starten
-load( "embed.rb" );
-
-$MM.addSoundsIntoPlayList();
-$MM.setLooping(true);
-$MM.setAuto(true);
-$MM.playSong();
--- modules/minidemo/scripts/zumpi.rb
+++
@@ -1,2 +0,0 @@
-zumpi = $AM.createParticleSystemActor("thezumpeffect","zumpi");
-zumpi.setPosition(-128.0,128.0,24.0);
