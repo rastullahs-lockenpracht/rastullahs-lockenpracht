2cbc556f148413ab63d94c28e28e4fdff9903bce
Sascha Kolewa <blakharaz@dsa-hl.de>
2008-03-05 16:13:55 +0000

    Removed all the output from MapLoadingTest Added animation map to TestPerson
    git-svn-id: svn://svn.berlios.de/dsa-hl@4154 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/regressiontest/scripts/MapLoadingTest.rb
+++ modules/regressiontest/scripts/MapLoadingTest.rb
@@ -8,35 +8,27 @@ class MapLoadingTest < TestCase
         loader = MapLoader.new("regressiontest");
         loader.loadMap("regressiontest_maploadingtest.rlmap.xml");
         
-		p "--- Built-in GO class, created by MapLoader ---"
+		# --- Built-in GO class, created by MapLoader ---
         builtIn = GameObjectManager.getSingleton().getGameObject(10)
-        p builtIn
-        p builtIn
-        p builtIn.getActor()
 
-		p "--- Custom GO class, created by MapLoader ---"
-        custom = GameObjectManager.getSingleton().getGameObject(11)
-        p custom
-        p custom
-        p custom.getActor()
-        
-		p "--- Built-in GO class, created by Ruby script ---"
+		# --- Built-in GO class, created by Ruby script ---"
         builtIn2 = GameObjectManager.getSingleton().createGameObject("builtin_go", 10)
         builtIn2.setName(builtIn2.getName() + "_2")
         builtIn2.setPosition([-6, 0, -10.0])
         builtIn2.setState(GOS_IN_SCENE)
-        p builtIn2
-        p builtIn2
-        p builtIn2.getActor()
+        assertEquals(builtIn, builtIn2)
+        assertEquals(builtIn.getActor(), builtIn2.getActor())
 
-		p "--- Custom GO class, created by Ruby script ---"
+		# --- Custom GO class, created by MapLoader ---
+        custom = GameObjectManager.getSingleton().getGameObject(11)
+        
+		# "--- Custom GO class, created by Ruby script ---"
         custom2 = GameObjectManager.getSingleton().createGameObject("custom_go", 11)
         custom2.setName(custom2.getName() + "_2")
         custom2.setPosition([-6, 0, -11.0])
         custom2.setState(GOS_IN_SCENE)
-        p custom2
-        p custom2
-        p custom2.getActor()
+        assertEquals(custom, custom2)
+        assertEquals(custom.getActor(), custom2.getActor())
 		
 		$SCRIPT.log("MapLoadingTest initialisiert")
     end
