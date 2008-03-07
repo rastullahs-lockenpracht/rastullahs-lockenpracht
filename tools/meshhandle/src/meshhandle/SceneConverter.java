c283135234c4b68351092ed19f0ad52b0bb86fd0
Henrik Hinrichs <henrik@mutaphysis.de>
2008-03-07 22:16:29 +0000

    Using last commandline argument as input file if no other was specified Converting in same directory as input file If no export version is given, using 0.4.0 Added error handling+output
    git-svn-id: svn://svn.berlios.de/dsa-hl@4223 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- tools/meshhandle/src/meshhandle/SceneConverter.java
+++ tools/meshhandle/src/meshhandle/SceneConverter.java
@@ -4,28 +4,27 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.xml.parsers.ParserConfigurationException;
+import javax.swing.JOptionPane;
 
 import meshhandle.io.xml.scene.AbstractSceneWriter;
 import meshhandle.io.xml.scene.ISceneLoader;
 import meshhandle.io.xml.scene.SceneFormat3Loader;
+import meshhandle.io.xml.scene.SceneFormat4Writer;
 import meshhandle.model.scene.Scene;
-
-import org.xml.sax.SAXException;
 
 public class SceneConverter
 {
 	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws SAXException 
-	 * @throws ParserConfigurationException 
+	 * Opens and reads a xml-file 
+	 *   --input
 	 */
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException
+	public static void main(String[] args)
 	{
 		String infile = null;
 		String outfile = null;
 		String version = null;
+		
+		boolean infilefound = false;
 
 		for (int idx = 0; idx < args.length; idx++)
 		{
@@ -33,6 +32,7 @@ public class SceneConverter
 			if (curParam.equals("--input"))
 			{
 				infile = args[idx+1];
+				infilefound = true;
 				idx++;
 			}
 			else if (curParam.equals("--output"))
@@ -45,23 +45,86 @@ public class SceneConverter
 				version = args[idx+1];
 				idx++;
 			}
+			// All unused paramters are used as infiles
+			else if( !infilefound )
+			{
+				infile = curParam;
+			}
 		}
 		
-		if (infile == null || version == null)
+		if( version == null )
 		{
-			System.exit(0);
+			version = SceneFormat4Writer.FORMAT_STRING;
+		}
+		
+		if (infile == null )
+		{
+			showError( "No input file found" );
 		}
 		
 		if (outfile == null)
 		{
-			outfile = infile.substring(0, infile.lastIndexOf(".")) + "_out.xml";
+			File in = new File(infile);
+			String inname = in.getName();
+			String dirname = in.getParentFile().getAbsolutePath();
+			outfile = inname.substring(0, inname.lastIndexOf(".")) + "_out.xml";
+			outfile = dirname+"/"+outfile;
 		}
 		
 		ISceneLoader sceneLoader = new SceneFormat3Loader();
-		Scene scene = sceneLoader.readScene(infile);
-		FileWriter outputWriter = new FileWriter(new File(outfile));
+		Scene scene = null;
+		FileWriter outputWriter = null;
+		
+		try
+		{
+			scene = sceneLoader.readScene(infile);
+		} 
+		catch (Throwable t )
+		{
+			showError( "Could not read scene" );
+		}
+		
+		try
+		{
+			outputWriter = new FileWriter(new File(outfile));
+		} 
+		catch (Throwable t )
+		{
+			showError( "Could not initialize outfile" );
+		}
+		
 		AbstractSceneWriter sceneWriter = AbstractSceneWriter.createSceneWriter(version, outputWriter);
-		sceneWriter.write(scene);
-		outputWriter.close();
+		
+		if( sceneWriter == null )
+		{
+			showError( "No matching SceneWriter found" );
+		}
+		
+		try
+		{
+			sceneWriter.write(scene);
+		} 
+		catch (IOException e)
+		{
+			showError( "Could not write to outfile" );
+		}
+		finally
+		{
+			try
+			{
+				outputWriter.close();
+			} 
+			catch (IOException e) {}
+		}
+	}
+
+	private static void showError(String message)
+	{
+		JOptionPane.showMessageDialog(
+				null, 
+				message, 
+				"Error",
+				JOptionPane.ERROR_MESSAGE );
+		System.exit(0);
 	}
 }
