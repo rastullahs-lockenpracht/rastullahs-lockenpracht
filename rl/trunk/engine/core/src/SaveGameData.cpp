804b32e55437e9700f456aa088f9a2b3a54762c3
Sascha Kolewa <blakharaz@dsa-hl.de>
2008-03-20 19:44:21 +0000

    Separate SaveGameData and SaveGameManager
    git-svn-id: svn://svn.berlios.de/dsa-hl@4302 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ rl/trunk/engine/core/src/SaveGameData.cpp
@@ -0,0 +1,11 @@
+/*
+ *  SaveGameData.cpp
+ *  Rastullah
+ *
+ *  Created by Sascha Kolewa on 19.03.08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "SaveGameData.h"
+
