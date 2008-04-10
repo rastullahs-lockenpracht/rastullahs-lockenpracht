450b9da4ce91501ee0f4c2d73f89e3d67c1ae88c
Timm Eversmeyer <Saeugetier@gmx.net>
2008-04-10 19:43:35 +0000

    fixed equip script
    git-svn-id: svn://svn.berlios.de/dsa-hl@4322 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/kanalisation/scripts/equip.rb
+++ modules/kanalisation/scripts/equip.rb
@@ -9,18 +9,15 @@ $SCRIPT.log("backpack erstellen");
 #backpack.setPosition(center);
 #backpack.placeIntoScene();
 
-#$SCRIPT.log("inventar füllen!!!");
+#$SCRIPT.log("inventar fuellen!!!");
 #hero.getInventory().hold(backpack, "Back");
 
-itemToAdd = $GOM.createGameObject("men_human_female_legs_pants_01_long");
+itemToAdd = $GOM.createGameObject("men_human_female_leg_pants_01_long");
 hero.getInventory().hold(itemToAdd, "Trousers");
-#hero.getInventory().getItem("Back").addItem(itemToAdd);
-itemToAdd = $GOM.createGameObject("men_human_female_cloth_short");
-hero.getInventory().hold(itemToAdd, "Clothes");
-#hero.getInventory().getItem("Back").addItem(itemToAdd);
+#itemToAdd = $GOM.createGameObject("men_human_female_cloth_short");
+#hero.getInventory().hold(itemToAdd, "Clothes");
 itemToAdd = $GOM.createGameObject("men_human_female_feet_boots_01");
 hero.getInventory().hold(itemToAdd, "Boots");
-# hero.getInventory().getItem("Back").addItem(itemToAdd);
 
 itemToAdd = $GOM.createGameObject("torch");
 hero.getInventory().hold(itemToAdd, "Right Hand");
