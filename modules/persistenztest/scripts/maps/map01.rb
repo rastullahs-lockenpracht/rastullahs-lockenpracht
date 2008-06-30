08e033cbdbce1ee7f756ecd8289cefef0d6aabd9
Timm Eversmeyer <Saeugetier@gmx.net>
2008-06-30 13:07:24 +0000

    changed layout in persistence test
    git-svn-id: svn://svn.berlios.de/dsa-hl@4411 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/persistenztest/scripts/maps/map01.rb
+++ modules/persistenztest/scripts/maps/map01.rb
@@ -1,10 +1,10 @@
 require 'embed.rb'
 require 'MapChange.rb'
 
-base = $AM.createBoxPrimitiveActor("SelectorTestBase", [-5,0.05,-5], [5,0.3,5], "alpha_blue")
+base = $AM.createBoxPrimitiveActor("SelectorTestBase", [-2,0.05,-2], [2,0.3,2], "alpha_blue")
 
 base.placeIntoScene([8,0,8])
 
 CreateSceneChangeTrigger("map1", [8,0,8], [0,0,0,1], [2, 2, 2], "scene02")
 
-#puts 'Map 1 init script wurde ausgeführt!'
+puts 'Map 1 init script wurde ausgeführt!'
