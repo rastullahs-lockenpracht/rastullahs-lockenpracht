f766545f98e22b2d87a5c2671f3dee49e6b3b5ab
Simon Hartmann <synter@wsfclan.de>
2008-07-02 21:39:21 +0000

    - new sewer -
    git-svn-id: svn://svn.berlios.de/dsa-hl@4429 4c79e8ff-cfd4-0310-af45-a38c79f83013

---
+++ modules/kanalisation/scripts/equip.rb
@@ -0,0 +1,17 @@
+require 'npc.rb'
+require 'torch.rb'
+require 'clothing.rb'
+
+hero = $UI.getActiveCharacter();
+
+itemToAdd = $GOM.createGameObject("men_human_female_legs_pants_01_long");
+hero.getInventory().hold(itemToAdd, "Trousers");
+
+itemToAdd = $GOM.createGameObject("men_human_female_feet_boots_01");
+hero.getInventory().hold(itemToAdd, "Boots");
+
+itemToAdd = $GOM.createGameObject("men_human_female_cloth_short");
+hero.getInventory().hold(itemToAdd, "Clothes");
+
+#itemToAdd = $GOM.createGameObject("torch");
+#hero.getInventory().hold(itemToAdd, "Right Hand");
