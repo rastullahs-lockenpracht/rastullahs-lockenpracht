d67a338cef4348d3443b7cf1897f42b4ace027e8
Sascha Kolewa <blakharaz@dsa-hl.de>
2008-10-03 07:31:12 +0000

    Show Game Over Window when the whole party is dead
    git-svn-id: svn://svn.berlios.de/dsa-hl@4543 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/engine/ui/src/UiSubsystem.cpp
+++ rl/trunk/engine/ui/src/UiSubsystem.cpp
@@ -62,6 +62,7 @@ namespace rl {
         mGuiResourceProvider(NULL),
         mGuiSystem(NULL)
     {
+        mWindowFactory = new WindowFactory();
         mSceneClearingConnection =
             MessagePump::getSingleton().addMessageHandler<MessageType_SceneClearing>(
 			    boost::bind(&UiSubsystem::onBeforeClearScene, this));
@@ -74,7 +75,9 @@ namespace rl {
         mActiveCharacterChangedConnection = 
             MessagePump::getSingleton().addMessageHandler<MessageType_ActivePlayerCharChanged>(
                 boost::bind(&UiSubsystem::onActiveCharacterChanged, this, _1, _2));
-        mWindowFactory = new WindowFactory();
+        mAllPlayerCharactersDiedConnection =
+            MessagePump::getSingleton().addMessageHandler<MessageType_AllPlayerCharsDied>(
+                boost::bind(&UiSubsystem::onAllPlayerCharactersDied, this));
     }
 
     UiSubsystem::~UiSubsystem()
@@ -98,7 +101,7 @@ namespace rl {
         World* world = CoreSubsystem::getSingleton().getWorld();
         SceneManager* sceneMgr = world->getSceneManager();
         
-        CEGUI::System::setDefaultXMLParserName("XercesParser");
+//        CEGUI::System::setDefaultXMLParserName("XercesParser");
 
         LOG_MESSAGE2(Logger::UI,
             "Initializing CEGUI Renderer.", "UiSubsystem::initializeUiSubsystem");
