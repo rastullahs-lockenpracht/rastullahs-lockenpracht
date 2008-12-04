f6da85c37cc8ba5c112a2f1e1c000c76060b9f58
Sascha Kolewa <blakharaz@dsa-hl.de>
2008-12-04 21:35:17 +0000

    Test starting a fight from within a dialog
    git-svn-id: svn://svn.berlios.de/dsa-hl@4634 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- modules/regressiontest/scripts/CombatTest.rb
+++ modules/regressiontest/scripts/CombatTest.rb
@@ -4,12 +4,30 @@ class CombatTest < TestCase
     def execute()
 		MapLoader.new().loadMap("combatarena.rlmap");
 		
+		insertWolf();
+		insertTestDialog();
+	end
+	
+	def insertWolf()
 		wolf = $GOM.getGameObject(100);
+		
 		wolf.setAlignment(Creature::ALIGNMENT_ENEMY);
+		
 		wolf.modifyAu(1000)
 
+		
 		schnauze = $GOM.createGameObject("Wolfsschnauze");
+		
 		wolf.getInventory().ready(schnauze, "Snout");
 
+	end
+	
+	def insertTestDialog()
+		npc = $GOM.createGameObject("TestPerson");
+		npc.setProperty("dialog", "combatdialog");
+		npc.getInventory().ready($GOM.createGameObject("Kurzschwert"), "Right Hand");
+
+		npc.setPosition(getCenter());
+		npc.placeIntoScene();
 	end
 end
