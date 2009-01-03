8de97fac3adb8471272cd007e7058065d18d4f51
Christoph Berg <Christoph.Berg@kpm-sport.de>
2009-01-03 22:40:12 +0000

    Corrected wrong cache variable resulting in always displaying of library and include variable values for OgreNewt Added installation of fmod4driver Copied CMakeLists.txt for plugins from trunk Corrected the name for the preprocessor libRlUi symbol
    git-svn-id: svn://svn.berlios.de/dsa-hl@4670 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/branches/newton20/cmake/FindOgreNewt.cmake
+++ rl/branches/newton20/cmake/FindOgreNewt.cmake
@@ -1,17 +1,17 @@
 # Find OgreNewt (Newton physic library bindings for OGRE)
 #
 # This module defines
-# OGRENEWT_FOUND
-# OGRENEWT_INCLUDE_DIR
-# OGRENEWT_LIBRARY
+# OGRENEWT_FOUND       - OgreNewt was found
+# OGRENEWT_INCLUDE_DIR - Directory containing OgreNewt header files
+# OGRENEWT_LIBRARY     - Library name of OgreNewt library
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 # On a new cmake run, we do not need to be verbose
-IF(OGRENEWT_INCLUDE_DIRS AND OGRENEWT_LIBRARY)
+IF(OGRENEWT_INCLUDE_DIR AND OGRENEWT_LIBRARY)
 	SET(OGRENEWT_FOUND_QUIETLY TRUE)
-ENDIF(OGRENEWT_INCLUDE_DIRS AND OGRENEWT_LIBRARY)
+ENDIF(OGRENEWT_INCLUDE_DIR AND OGRENEWT_LIBRARY)
 
 IF(WIN32)
 
