72bd68331f3787afb27e56f1b18d24941edf6bbd
Stefan Stammberger <sstammberger@web.de>
2009-03-20 19:48:23 +0000

    - Lockenwickler license headers changed to be GPL now
    git-svn-id: svn://svn.berlios.de/dsa-hl@4855 4c79e8ff-cfd4-0310-af45-a38c79f83013

--- rl/trunk/editors/Lockenwickler/src/ui_ConsoleWindow.py
+++ rl/trunk/editors/Lockenwickler/src/ui_ConsoleWindow.py
@@ -1,11 +1,21 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'console_window.ui'
+ #################################################
+# This source file is part of Rastullahs Lockenwickler.
+# Copyright (C) 2003-2009 Team Pantheon. http://www.team-pantheon.de
 #
-# Created: Mon May 12 15:09:24 2008
-#      by: PyQt4 UI code generator 4.3.3
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# WARNING! All changes made in this file will be lost!
+# This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ #################################################
 
 from PyQt4 import QtCore, QtGui
 
